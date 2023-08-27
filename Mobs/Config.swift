ebb1d8a9b21595dabbae2cdf115e059b80e60040
Erik Moqvist <erik.moqvist@gmail.com>
2023-08-27 15:49:58 +0200

    Doing stuff.

--- Mobs/Config.swift
+++ Mobs/Config.swift
@@ -2,7 +2,7 @@ struct Config {
     var uri: String = "rtmp://192.168.50.72:1935/live"
     var streamName: String = "1234"
     var twitchChatChannel: String = "jinnytty"
-    var twitchChannelId: String = "558738387"
+    var twitchChannelId: String = "59965916"
 }
 
 var defaultConfig = Config()
