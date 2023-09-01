9d950141cdca2cb52869bc33286aca85e2d0b298
Erik Moqvist <erik.moqvist@gmail.com>
2023-09-01 22:41:18 +0200

    srtla continued.

--- Mobs/Srtla.swift
+++ Mobs/Srtla.swift
@@ -32,7 +32,7 @@ class SrtlaType {
         params.prohibitExpensivePaths = false
         connection = NWConnection(host: "mys-lang.org", port: 443, using: params)
         if let connection = connection {
-            print(typeName, connection)
+            // print(typeName, connection)
             connection.viabilityUpdateHandler = viabilityDidChange(to:)
             connection.stateUpdateHandler = stateDidChange(to:)
             connection.start(queue: networkQueue)
@@ -56,12 +56,69 @@ class SrtlaType {
     }
 }
 
-class Srtla {
-    private var cellular: SrtlaType = SrtlaType(typeName: "cellular", type: .cellular)
-    private var wifi: SrtlaType = SrtlaType(typeName: "wifi", type: .wifi)
-    private var wiredEthernet: SrtlaType = SrtlaType(typeName: "wiredEthernet", type: .wiredEthernet)
-
+class LocalListener {
+    private var networkQueue = DispatchQueue(label: "com.eerimoq.network", qos: .userInitiated)
+    private var listener: NWListener?
+    var localPort: NWEndpoint.Port? = nil
+    
     func start() {
+        do {
+            listener = try NWListener(using: .udp, on: .any)
+        } catch {
+            print("Failed to create SRTLA listener with error", error)
+            return
+        }
+        listener!.stateUpdateHandler = stateDidChange(to:)
+        listener!.newConnectionHandler = newConnectionHandler
+        listener!.start(queue: networkQueue)
+    }
+
+    func stop() {
+        if let listener = listener {
+            listener.cancel()
+        }
+    }
+
+    private func stateDidChange(to state: NWListener.State) {
+        // print("Listener state changed to", state)
+        switch state {
+        case .ready:
+            if let port = listener!.port {
+                print("Listener at port", port)
+                localPort = port
+            }
+        default:
+            break
+        }
+    }
+    
+    func newConnectionHandler(connection: NWConnection) {
+        print("New connection", connection.debugDescription)
+        connection.stateUpdateHandler = { (state) in
+            print("Connection state", state)
+            switch state {
+            case .ready:
+                print("Connection ready")
+            case .failed(let error):
+                print("Connection failed with error", error)
+            case .cancelled:
+                print("Connection cancelled")
+            default:
+                break
+            }
+        }
+        connection.start(queue: networkQueue)
+    }
+}
+
+class Srtla {
+    private var cellular = SrtlaType(typeName: "cellular", type: .cellular)
+    private var wifi = SrtlaType(typeName: "wifi", type: .wifi)
+    private var wiredEthernet = SrtlaType(typeName: "wiredEthernet", type: .wiredEthernet)
+    private var localListener = LocalListener()
+    
+    func start() {
+        localListener.start()
         cellular.start()
         wifi.start()
         wiredEthernet.start()
@@ -71,5 +128,6 @@ class Srtla {
         cellular.stop()
         wifi.stop()
         wiredEthernet.stop()
+        localListener.start()
     }
 }
