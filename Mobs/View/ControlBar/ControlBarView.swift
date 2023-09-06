04d51cf5d9cca457a5ed617b5a0aa09af9ee3ff7
Erik Moqvist <erik.moqvist@gmail.com>
2023-09-06 08:27:57 +0200

    Cleanup.

--- Mobs/View/ControlBar/ControlBarView.swift
+++ Mobs/View/ControlBar/ControlBarView.swift
@@ -75,7 +75,7 @@ struct ControlBarView: View {
                     Image("AppIconNoBackground")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .padding([.bottom], 6)
+                        .padding([.bottom], 5)
                         .frame(width: 40, height: 40)
                     Button(action: {
                         logger.error("Settings")
