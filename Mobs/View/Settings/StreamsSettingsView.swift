de613f0777a88952f20359046e63ce10ae1887fd
Erik Moqvist <erik.moqvist@gmail.com>
2023-09-09 04:59:13 +0200

    Streams footer.

--- Mobs/View/Settings/StreamsSettingsView.swift
+++ Mobs/View/Settings/StreamsSettingsView.swift
@@ -12,34 +12,38 @@ struct StreamsSettingsView: View {
     var body: some View {
         VStack {
             Form {
-                ForEach(database.streams) { stream in
-                    NavigationLink(destination: StreamSettingsView(stream: stream, model: model)) {
-                        Toggle(stream.name, isOn: Binding(get: {
-                            stream.enabled
-                        }, set: { value in
-                            stream.enabled = value
-                            for ostream in database.streams {
-                                if ostream.id != stream.id {
-                                    ostream.enabled = false
+                Section {
+                    ForEach(database.streams) { stream in
+                        NavigationLink(destination: StreamSettingsView(stream: stream, model: model)) {
+                            Toggle(stream.name, isOn: Binding(get: {
+                                stream.enabled
+                            }, set: { value in
+                                stream.enabled = value
+                                for ostream in database.streams {
+                                    if ostream.id != stream.id {
+                                        ostream.enabled = false
+                                    }
                                 }
-                            }
-                            model.store()
-                            model.reloadStream()
-                            model.objectWillChange.send()
-                        }))
-                        .disabled(stream.enabled)
+                                model.store()
+                                model.reloadStream()
+                                model.objectWillChange.send()
+                            }))
+                            .disabled(stream.enabled)
+                        }
                     }
+                    .onDelete(perform: { offsets in
+                        database.streams.remove(atOffsets: offsets)
+                        model.store()
+                        model.objectWillChange.send()
+                    })
+                    CreateButtonView(action: {
+                        database.streams.append(SettingsStream(name: "My stream"))
+                        model.store()
+                        model.objectWillChange.send()
+                    })
+                } footer: {
+                    Text("Only one stream can be used at a time.")
                 }
-                .onDelete(perform: { offsets in
-                    database.streams.remove(atOffsets: offsets)
-                    model.store()
-                    model.objectWillChange.send()
-                })
-                CreateButtonView(action: {
-                    database.streams.append(SettingsStream(name: "My stream"))
-                    model.store()
-                    model.objectWillChange.send()
-                })
             }
         }
         .navigationTitle("Streams")
