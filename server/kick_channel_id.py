6d06c76a45017bd497ff0d8f3028a980494aff38
Erik Moqvist <erik.moqvist@gmail.com>
2023-09-22 19:47:17 +0200

    Chatroom id.

--- server/kick_channel_id.py
+++ server/kick_channel_id.py
@@ -10,16 +10,23 @@ RE_BODY = re.compile(r'^<html><head></head><body>(.*)</body></html>$')
 
 
 def main():
-    user = sys.argv[1]
     options = Options()
     options.add_argument('-headless')
     driver = webdriver.Firefox(options=options)
-    driver.get(f"https://kick.com/api/v1/channels/{user}")
-    page_source = driver.page_source
+
+    while True:
+        user = sys.stdin.readline().strip()
+
+        if not user:
+            break
+
+        driver.get(f"https://kick.com/api/v1/channels/{user}")
+        page_source = driver.page_source
+        info = json.loads(RE_BODY.match(page_source).group(1))
+        channel_id = info['chatroom']['id']
+        print(channel_id)
+
     driver.quit()
-    info = json.loads(RE_BODY.match(page_source).group(1))
-    channel_id = info['chatroom']['id']
-    print(channel_id)
 
 
 main()
