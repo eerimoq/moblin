ae8da587fef8e57caf370b08bc0d1630ffbdad43
Erik Moqvist <erik.moqvist@gmail.com>
2023-10-09 06:50:50 +0200

    Fix deprecated windows.

--- Mobs/MobsApp.swift
+++ Mobs/MobsApp.swift
@@ -17,11 +17,16 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 
     static func setAllowedOrientations(mask: UIInterfaceOrientationMask) {
         AppDelegate.orientationLock = mask
-        if UIApplication.shared.windows.first?.rootViewController?
+        if getWindow()?.rootViewController?
             .setNeedsUpdateOfSupportedInterfaceOrientations() == nil
         {
             logger.error("app: Failed to set allowed orientations. No window.")
         }
+    }
+
+    private static func getWindow() -> UIWindow? {
+        return UIApplication.shared.connectedScenes
+            .flatMap { ($0 as? UIWindowScene)?.windows ?? [] }.last
     }
 
     func application(_: UIApplication,
