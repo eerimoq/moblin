2a81550e1018859898124629d43dda62461510f3
Erik Moqvist <erik.moqvist@gmail.com>
2023-10-14 09:35:35 +0200

    Mention mobs:// on import/export settings page.

---
+++ Mobs/View/Settings/ImportExport/ImportExportSettingsView.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct ImportExportSettingsView: View {
+    @ObservedObject var model: Model
+
+    var body: some View {
+        Form {
+            Section {
+                ImportSettingsView(model: model)
+            }
+            Section {
+                ExportSettingsView(model: model)
+            } footer: {
+                VStack(alignment: .leading) {
+                    Text("")
+                    Text("""
+                    Do not share your settings with anyone as they may contain \
+                    sensitive data (stream keys, etc.)!
+                    """).bold()
+                    Text("")
+                    Text("""
+                    mobs:// can be used to import some settings, often using QR \
+                    codes or browser. See https://github.com/eerimoq/mobs for details.
+                    """)
+                }
+            }
+        }
+        .navigationTitle("Import and export settings")
+    }
+}
