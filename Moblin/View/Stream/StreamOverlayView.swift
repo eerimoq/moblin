75b8215881371beaa515878c0101f03cb84d8730
Erik Moqvist <erik.moqvist@gmail.com>
2023-11-07 07:09:55 +0100

    Make chat paused warning ignore touch.

--- Moblin/View/Stream/StreamOverlayView.swift
+++ Moblin/View/Stream/StreamOverlayView.swift
@@ -66,6 +66,7 @@ struct StreamOverlayView: View {
                                 Spacer()
                             }
                         }
+                        .allowsHitTesting(false)
                     }
                 }
             }
