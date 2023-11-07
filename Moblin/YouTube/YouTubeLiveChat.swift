54fdb8ffdf16fa2c45b9ac3f89f8b3e7b97b5d8f
Erik Moqvist <erik.moqvist@gmail.com>
2023-11-07 22:54:59 +0100

    A first, useless, implementation of YouTube chat.

--- Moblin/YouTube/YouTubeLiveChat.swift
+++ Moblin/YouTube/YouTubeLiveChat.swift
@@ -1,18 +1,30 @@
 import Foundation
 
-private struct Snippet: Codable {
+private struct MessagesSnippet: Codable {
     var authorChannelId: String
     var displayMessage: String
 }
 
-private struct Item: Codable {
-    var snippet: Snippet
+private struct MessagesItem: Codable {
+    var snippet: MessagesSnippet
 }
 
 private struct Messages: Codable {
     var pollingIntervalMillis: UInt64
     var nextPageToken: String
-    var items: [Item]
+    var items: [MessagesItem]
+}
+
+private struct ChannelSnippet: Codable {
+    var title: String
+}
+
+private struct ChannelItem: Codable {
+    var snippet: ChannelSnippet
+}
+
+private struct Channel: Codable {
+    var items: [ChannelItem]
 }
 
 final class YouTubeLiveChat: NSObject {
@@ -22,6 +34,7 @@ final class YouTubeLiveChat: NSObject {
     private var nextToken: String?
     private var task: Task<Void, Error>?
     private var emotes: Emotes
+    private var channelToTitle: [String: String] = [:]
 
     init(model: Model, apiKey: String, liveChatId: String) {
         self.model = model
@@ -30,7 +43,7 @@ final class YouTubeLiveChat: NSObject {
         emotes = Emotes()
     }
 
-    private func makeUrl() -> URL? {
+    private func makeMessagesUrl() -> URL? {
         var url = """
         https://www.googleapis.com/youtube/v3/liveChat/messages\
         ?part=id,snippet&key=\(apiKey)&liveChatId=\(liveChatId)
@@ -41,12 +54,38 @@ final class YouTubeLiveChat: NSObject {
         return URL(string: url)
     }
 
+    private func makeChannelsUrl(channelId: String) -> URL? {
+        return URL(string: """
+        https://www.googleapis.com/youtube/v3/channels\
+        ?part=snippet&key=\(apiKey)&id=\(channelId)
+        """)
+    }
+
+    private func getChannelTitle(channelId: String) async throws -> String {
+        if let title = channelToTitle[channelId] {
+            return title
+        }
+        guard let url = makeChannelsUrl(channelId: channelId) else {
+            return "???"
+        }
+        let (data, response) = try await httpGet(from: url)
+        if response.isSuccessful {
+            let channel = try JSONDecoder().decode(Channel.self, from: data)
+            if channel.items.count != 1 {
+                return "???"
+            }
+            channelToTitle[channelId] = channel.items[0].snippet.title
+            return channel.items[0].snippet.title
+        }
+        return "???"
+    }
+
     func start() {
         task = Task.init {
             var pollingIntervalMillis: UInt64 = 1000
             do {
                 while true {
-                    guard let url = makeUrl() else {
+                    guard let url = makeMessagesUrl() else {
                         break
                     }
                     let (data, response) = try await httpGet(from: url)
@@ -57,9 +96,11 @@ final class YouTubeLiveChat: NSObject {
                         for item in messages.items {
                             let segments = createSegments(message: item.snippet
                                 .displayMessage)
+                            let user = try await getChannelTitle(channelId: item.snippet
+                                .authorChannelId)
                             await MainActor.run {
                                 self.model.appendChatMessage(
-                                    user: item.snippet.authorChannelId,
+                                    user: user,
                                     userColor: nil,
                                     segments: segments,
                                     timestamp: model.digitalClock
