e1f1637a0138dbb75489587c366d3774d27e79f7
Erik Moqvist <erik.moqvist@gmail.com>
2023-11-18 20:34:48 +0100

    Bitrate script.

--- utils/bitrate.sh
+++ utils/bitrate.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+minimum=1.0
+
 while true ; do
-    speed=$(python3 -c "import random ; print(round(random.random()* 10 + 0.5), end='')")
+    speed=$(python3 -c "import random ; print(round(random.random()* 10 + ${minimum}, 1), end='')")
     date | tr -d "\n"
     echo " - ${speed} Mbit"
     sudo tc qdisc replace dev eno1 root netem rate ${speed}Mbit
