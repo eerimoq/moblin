e92ccf34b2109e8575847512944404add2066a8b
Erik Moqvist <erik.moqvist@gmail.com>
2023-11-18 12:33:17 +0100

    Cleanup and bitrate script.

--- utils/bitrate.sh
+++ utils/bitrate.sh
@@ -2,7 +2,8 @@
 
 while true ; do
     speed=$(python3 -c "import random ; print(round(random.random()* 10 + 0.5), end='')")
-    echo ${speed} Mbit
+    date | tr -d "\n"
+    echo " - ${speed} Mbit"
     sudo tc qdisc replace dev eno1 root netem rate ${speed}Mbit
     sleep 15
 done
