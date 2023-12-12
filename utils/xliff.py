2e9dfdbe52563c107169f97bfbe60412947f03ac
Erik Moqvist <erik.moqvist@gmail.com>
2023-12-12 08:01:07 +0100

    xliff script

--- utils/xliff.py
+++ utils/xliff.py
@@ -1,14 +1,32 @@
 import argparse
 from xml.etree import ElementTree
 
+NS = {
+    '': "urn:oasis:names:tc:xliff:document:1.2"
+}
+
+ElementTree.register_namespace('', "urn:oasis:names:tc:xliff:document:1.2")
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('xliff')
     args = parser.parse_args()
 
-    ElementTree.register_namespace('', "urn:oasis:names:tc:xliff:document:1.2")
-
     tree = ElementTree.parse(args.xliff)
+
+    for body in tree.findall('./file/body', namespaces=NS):
+        sorted_trans_units = []
+
+        for trans_unit in body.findall('./trans-unit', namespaces=NS):
+            if trans_unit.find("./target[@state='translated']", namespaces=NS) is not None:
+                sorted_trans_units.append(trans_unit)
+            else:
+                sorted_trans_units.insert(0, trans_unit)
+
+        body.clear()
+        body.extend(sorted_trans_units)
+
+    ElementTree.indent(tree)
     tree.write(args.xliff, xml_declaration=True, encoding='utf-8')
 
 
