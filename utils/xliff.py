e01dd0efec26a89f35b1256f1854b62be65024bc
Erik Moqvist <erik.moqvist@gmail.com>
2023-12-15 18:19:21 +0100

    Translations helpers.

--- utils/xliff.py
+++ utils/xliff.py
@@ -18,10 +18,15 @@ def main():
         sorted_trans_units = []
 
         for trans_unit in body.findall('./trans-unit', namespaces=NS):
-            if trans_unit.find("./target", namespaces=NS) is not None:
-                sorted_trans_units.append(trans_unit)
-            else:
+            target = trans_unit.find("./target", namespaces=NS)
+
+            if target is None:
                 sorted_trans_units.insert(0, trans_unit)
+            else:
+                if target.attrib.get('state') == 'translated':
+                    sorted_trans_units.append(trans_unit)
+                else:
+                    sorted_trans_units.insert(0, trans_unit)
 
         body.clear()
         body.extend(sorted_trans_units)
