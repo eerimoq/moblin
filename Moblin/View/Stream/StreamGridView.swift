bb09d731bd009148a28020f8fa462d237e65c07b
Erik Moqvist <erik.moqvist@gmail.com>
2024-01-12 07:28:10 +0100

    Refactoring.

--- Moblin/View/Stream/StreamGridView.swift
+++ Moblin/View/Stream/StreamGridView.swift
@@ -1,28 +1,24 @@
 import SwiftUI
 
 struct StreamGridView: View {
-    func draw(context: GraphicsContext, size: CGSize) {
-        let height = size.height / 3
-        let width = size.width / 3
-        var path = Path()
-        path.move(to: CGPoint(x: width, y: 0))
-        path.addLine(to: CGPoint(x: width, y: size.height))
-        path.move(to: CGPoint(x: 2 * width, y: 0))
-        path.addLine(to: CGPoint(x: 2 * width, y: size.height))
-        path.move(to: CGPoint(x: 0, y: height))
-        path.addLine(to: CGPoint(x: size.width, y: height))
-        path.move(to: CGPoint(x: 0, y: 2 * height))
-        path.addLine(to: CGPoint(x: size.width, y: 2 * height))
-        context.stroke(path, with: .color(.gray), lineWidth: 1.5)
-    }
-
     var body: some View {
-        GeometryReader { metrics in
-            Canvas { context, _ in
-                draw(context: context, size: metrics.size)
-            }
-            .ignoresSafeArea()
+        Canvas { context, size in
+            let height = size.height / 3
+            let width = size.width / 3
+            var path = Path()
+            // Horizontal
+            path.move(to: CGPoint(x: 0, y: height))
+            path.addLine(to: CGPoint(x: size.width, y: height))
+            path.move(to: CGPoint(x: 0, y: 2 * height))
+            path.addLine(to: CGPoint(x: size.width, y: 2 * height))
+            // Vertical
+            path.move(to: CGPoint(x: width, y: 0))
+            path.addLine(to: CGPoint(x: width, y: size.height))
+            path.move(to: CGPoint(x: 2 * width, y: 0))
+            path.addLine(to: CGPoint(x: 2 * width, y: size.height))
+            context.stroke(path, with: .color(.gray), lineWidth: 1.5)
         }
+        .ignoresSafeArea()
         .allowsHitTesting(false)
     }
 }
