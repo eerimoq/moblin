3dbcfc88550d06ef274d3e60b6c81ada5c074ee6
Erik Moqvist <erik.moqvist@gmail.com>
2024-02-13 19:55:54 +0100

    Fix draw on stream when 16:9 video fills screen width but not height.

--- Moblin/VideoEffects/DrawOnStreamEffect.swift
+++ Moblin/VideoEffects/DrawOnStreamEffect.swift
@@ -26,8 +26,8 @@ private func calculateMidPoint(_ point1: CGPoint, _ point2: CGPoint) -> CGPoint 
     return CGPoint(x: (point1.x + point2.x) / 2, y: (point1.y + point2.y) / 2)
 }
 
-private func transformPoint(point: CGPoint, scale: Double, offsetX: Double) -> CGPoint {
-    return CGPoint(x: point.x * scale - offsetX, y: point.y * scale)
+private func transformPoint(point: CGPoint, scale: Double, offsetX: Double, offsetY: Double) -> CGPoint {
+    return CGPoint(x: point.x * scale - offsetX, y: point.y * scale - offsetY)
 }
 
 final class DrawOnStreamEffect: VideoEffect {
@@ -38,21 +38,36 @@ final class DrawOnStreamEffect: VideoEffect {
         DispatchQueue.main.async {
             let drawRatio = size.width / size.height
             let videoRatio = videoSize.width / videoSize.height
-            let offsetX = (drawRatio / videoRatio * videoSize.width - videoSize.width) / 2
-            let scale = videoSize.height / size.height
+            var offsetX: Double
+            var offsetY: Double
+            var scale: Double
+            if drawRatio > videoRatio {
+                offsetX = (drawRatio / videoRatio * videoSize.width - videoSize.width) / 2
+                offsetY = 0
+                scale = videoSize.height / size.height
+            } else {
+                offsetX = 0
+                offsetY = (videoRatio / drawRatio * videoSize.height - videoSize.height) / 2
+                scale = videoSize.width / size.width
+            }
             let canvas = Canvas { context, _ in
                 for line in lines {
                     let width = line.width * scale
                     if line.points.count > 1 {
                         context.stroke(
                             drawOnStreamCreatePath(points: line.points.map { point in
-                                transformPoint(point: point, scale: scale, offsetX: offsetX)
+                                transformPoint(point: point, scale: scale, offsetX: offsetX, offsetY: offsetY)
                             }),
                             with: .color(line.color),
                             lineWidth: width
                         )
                     } else {
-                        let point = transformPoint(point: line.points[0], scale: scale, offsetX: offsetX)
+                        let point = transformPoint(
+                            point: line.points[0],
+                            scale: scale,
+                            offsetX: offsetX,
+                            offsetY: offsetY
+                        )
                         var path = Path()
                         path.addEllipse(in: CGRect(x: point.x, y: point.y, width: 1, height: 1))
                         context.stroke(path, with: .color(line.color), lineWidth: width)
