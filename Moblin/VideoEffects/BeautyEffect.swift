93161e22797072a2f0e0060958946baf5b69ecfc
Erik Moqvist <erik.moqvist@gmail.com>
2024-04-27 15:50:58 +0200

    Beauty filter continued.

--- Moblin/VideoEffects/BeautyEffect.swift
+++ Moblin/VideoEffects/BeautyEffect.swift
@@ -4,7 +4,6 @@ import Vision
 
 final class BeautyEffect: VideoEffect {
     private let faceBlender = CIFilter.blendWithMask()
-    private let lipsBlender = CIFilter.blendWithMask()
 
     override func getName() -> String {
         return "beauty filter"
@@ -18,31 +17,36 @@ final class BeautyEffect: VideoEffect {
         guard let faceDetections else {
             return image
         }
-        let faceMaskImage = CIImage.empty().cropped(to: image.extent)
-        let lipsMaskImage = CIImage.empty().cropped(to: image.extent)
+        var faceMaskImage = CIImage.empty().cropped(to: image.extent)
         for faceDetection in faceDetections {
-            print("Face bounding box:", faceDetection.boundingBox)
-            /* if let landmarks = faceDetection.landmarks {
-                 if let faceContour = landmarks.faceContour {
-                     print("faceContour", faceContour.normalizedPoints)
-                 }
-                 if let innerLips = landmarks.innerLips {
-                     print("innerLips", innerLips.normalizedPoints)
-                 }
-                 if let outerLips = landmarks.outerLips {
-                     print("outerLips", outerLips.normalizedPoints)
-                 }
-                 if let medianLine = landmarks.medianLine {
-                     print("medianLine", medianLine.normalizedPoints)
-                 }
-             } */
+            let faceRect = CGRect(x: faceDetection.boundingBox.minX * image.extent.width,
+                                  y: faceDetection.boundingBox.minY * image.extent.height,
+                                  width: faceDetection.boundingBox.width * image.extent.width,
+                                  height: faceDetection.boundingBox.height * image.extent.height)
+            let maskCenter = CGPoint(x: faceRect.maxX - (faceRect.width / 2),
+                                     y: faceRect.maxY - (faceRect.height / 2))
+            let faceMaskFilter = CIFilter.radialGradient()
+            faceMaskFilter.center = maskCenter
+            faceMaskFilter.radius0 = Float(faceRect.height / 2)
+            faceMaskFilter.radius1 = Float(faceRect.height)
+            faceMaskFilter.color0 = CIColor.white
+            faceMaskFilter.color1 = CIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.0)
+            guard let gradientOutput = faceMaskFilter.outputImage?.cropped(to: faceRect.insetBy(
+                dx: -faceRect.width / 2,
+                dy: -faceRect.height / 2
+            )) else {
+                continue
+            }
+            faceMaskImage = gradientOutput.composited(over: faceMaskImage)
         }
-        faceBlender.inputImage = image
+        let blurRadius = 30.0
+        let blurredImage = image
+            .clampedToExtent()
+            .applyingGaussianBlur(sigma: blurRadius)
+            .cropped(to: image.extent)
+        faceBlender.inputImage = blurredImage
         faceBlender.backgroundImage = image
         faceBlender.maskImage = faceMaskImage
-        lipsBlender.inputImage = image
-        lipsBlender.backgroundImage = faceBlender.outputImage
-        lipsBlender.maskImage = lipsMaskImage
-        return lipsBlender.outputImage ?? image
+        return faceBlender.outputImage ?? image
     }
 }
