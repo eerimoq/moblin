445babc99fa2b6b5daf86de9d279ec87e6c6e927
Erik Moqvist <erik.moqvist@gmail.com>
2024-05-09 14:23:04 +0200

    Rist continued.

--- Moblin/HaishinKit/RIST/RistStream.swift
+++ Moblin/HaishinKit/RIST/RistStream.swift
@@ -24,13 +24,13 @@ class RistStream: NetStream {
             logger.info("rist: Failed to create context")
             return
         }
+        context.onStats = handleStats
         self.context = context
         if bonding {
             // To Do: Monitor available network inferfaces
-            // addPeer(url: "\(url)&miface=en0")
-            // addPeer(url: "\(url)&miface=pdp_ip0")
-            addPeer(url: "\(url)&miface=en0&weight=1")
-            addPeer(url: "\(url)&miface=pdp_ip0&weight=1")
+            addPeer(url: makeBondingUrl(url: url, interfaceName: "en0", weight: "5"))
+            // addPeer(url: makeBondingUrl(url: url, interfaceName: "en3", weight: "10"))
+            addPeer(url: makeBondingUrl(url: url, interfaceName: "pdp_ip0", weight: "1"))
         } else {
             addPeer(url: url)
         }
@@ -48,8 +48,31 @@ class RistStream: NetStream {
         writer.startRunning()
     }
 
-    func addPeer(url: String) {
-        guard let peer = context?.addPeer(url: url) else {
+    func makeBondingUrl(url: String, interfaceName: String, weight: String) -> String? {
+        guard let url = URL(string: url) else {
+            return nil
+        }
+        guard var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
+            return nil
+        }
+        urlComponents.query = url.query
+        var queryItems: [URLQueryItem] = urlComponents.queryItems ?? []
+        queryItems.append(URLQueryItem(name: "miface", value: interfaceName))
+        queryItems.append(URLQueryItem(name: "weight", value: weight))
+        urlComponents.queryItems = queryItems
+        return urlComponents.url?.absoluteString
+    }
+
+    func handleStats(stats: RistStats) {
+        print("""
+        rist: peer \(stats.sender.peerId), rtt \(stats.sender.rtt), quality \(stats.sender.quality), \
+        sent \(stats.sender.sentPackets), received \(stats.sender.receivedPackets), \
+        bandwidth \(stats.sender.bandwidth), retry bandwidth \(stats.sender.retryBandwidth)
+        """)
+    }
+
+    func addPeer(url: String?) {
+        guard let url, let peer = context?.addPeer(url: url) else {
             logger.info("rist: Failed to add peer")
             return
         }
