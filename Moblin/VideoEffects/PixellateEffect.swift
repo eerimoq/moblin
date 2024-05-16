2cdee82bde3ae1f9f0d9826067031b32f6e0bc24
Erik Moqvist <erik.moqvist@gmail.com>
2024-05-16 20:56:15 +0200

    Bigger pixels in pixellate filter.

--- Moblin/VideoEffects/PixellateEffect.swift
+++ Moblin/VideoEffects/PixellateEffect.swift
@@ -13,14 +13,18 @@ final class PixellateEffect: VideoEffect {
     override func execute(_ image: CIImage, _: [VNFaceObservation]?) -> CIImage {
         filter.inputImage = image
         filter.center = .init(x: 0, y: 0)
-        filter.scale = 10 * (Float(image.extent.width) / 1920)
+        filter.scale = 20 * (Float(image.extent.width) / 1920)
         return filter.outputImage?.cropped(to: image.extent) ?? image
     }
 
     override func executeMetalPetal(_ image: MTIImage?, _: [VNFaceObservation]?) -> MTIImage? {
+        guard let image else {
+            return nil
+        }
         let filter = MTIPixellateFilter()
         filter.inputImage = image
-        filter.scale = .init(width: 20, height: 20)
+        let scale = 20 * (image.extent.width / 1920)
+        filter.scale = .init(width: scale, height: scale)
         return filter.outputImage
     }
 
