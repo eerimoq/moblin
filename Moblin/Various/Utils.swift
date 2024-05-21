c081342385c955fc99db36fc2aa7144fe4c790fe
Erik Moqvist <erik.moqvist@gmail.com>
2024-05-21 19:11:15 +0200

    Fix crash for some ISO settings.

--- Moblin/Various/Utils.swift
+++ Moblin/Various/Utils.swift
@@ -402,7 +402,8 @@ func hasAppleLog() -> Bool {
 }
 
 func factorToIso(device: AVCaptureDevice, factor: Float) -> Float {
-    var iso = device.activeFormat.minISO + (device.activeFormat.maxISO - device.activeFormat.minISO) * factor.clamped(to: 0...1)
+    var iso = device.activeFormat.minISO + (device.activeFormat.maxISO - device.activeFormat.minISO) * factor
+        .clamped(to: 0 ... 1)
     if !iso.isFinite {
         iso = 0
     }
@@ -415,7 +416,7 @@ func factorFromIso(device: AVCaptureDevice, iso: Float) -> Float {
     if !factor.isFinite {
         factor = 0
     }
-    return factor.clamped(to: 0...1)
+    return factor.clamped(to: 0 ... 1)
 }
 
 let minimumWhiteBalanceTemperature: Float = 2200
