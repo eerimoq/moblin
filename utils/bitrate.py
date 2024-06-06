6b64e20b6325bff5480eeb59a7d4a0bd0a2ac88f
Erik Moqvist <erik.moqvist@gmail.com>
2024-06-06 20:55:28 +0200

    Utils.

--- utils/bitrate.py
+++ utils/bitrate.py
@@ -3,6 +3,7 @@
 import random
 import time
 import subprocess
+import argparse
 
 
 def set_speed(speed):
@@ -12,7 +13,20 @@ def set_speed(speed):
                    shell=True,
                    check=True)
 
-def do_random():
+
+def do_constant(args):
+    set_speed(args.bitrate)
+
+
+def do_square(args):
+    while True:
+        set_speed(args.lowbitrate)
+        time.sleep(15)
+        set_speed(args.highbitrate)
+        time.sleep(15)
+
+
+def do_random(args):
     minimum = 1.0
 
     while True:
@@ -21,14 +35,28 @@ def do_random():
         time.sleep(15)
 
 
-def do_constant():
-    set_speed(4)
-
-
 def main():
-    # do_random()
-    # do_square()
-    do_constant()
+    parser = argparse.ArgumentParser()
+
+    # Workaround to make the subparser required in Python 3.
+    subparsers = parser.add_subparsers(title='subcommands',
+                                       dest='subcommand')
+    subparsers.required = True
+
+    subparser = subparsers.add_parser('constant')
+    subparser.add_argument('bitrate', type=float)
+    subparser.set_defaults(func=do_constant)
+
+    subparser = subparsers.add_parser('square')
+    subparser.add_argument('lowbitrate', type=float)
+    subparser.add_argument('highbitrate', type=float)
+    subparser.set_defaults(func=do_square)
+
+    subparser = subparsers.add_parser('random')
+    subparser.set_defaults(func=do_random)
+
+    args = parser.parse_args()
+    args.func(args)
 
 
 main()
