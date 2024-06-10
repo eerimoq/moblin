ab06a15659c18c92a42e619ea6e656807807b2c7
Erik Moqvist <erik.moqvist@gmail.com>
2024-06-10 22:40:53 +0200

    Fix background streaming navigation.

--- Moblin/View/Settings/Streams/Stream/StreamSettingsView.swift
+++ Moblin/View/Settings/Streams/Stream/StreamSettingsView.swift
@@ -162,14 +162,12 @@ struct StreamSettingsView: View {
                 }
             }
             Section {
-                NavigationLink(destination: StreamObsSettingsView(stream: stream)) {
-                    Toggle("Background streaming", isOn: Binding(get: {
-                        stream.backgroundStreaming!
-                    }, set: { value in
-                        stream.backgroundStreaming = value
-                        model.store()
-                    }))
-                }
+                Toggle("Background streaming", isOn: Binding(get: {
+                    stream.backgroundStreaming!
+                }, set: { value in
+                    stream.backgroundStreaming = value
+                    model.store()
+                }))
             } footer: {
                 Text("Keep live streams running when the app is in background mode.")
             }
