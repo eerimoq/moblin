9886bb29688d60af841517dd733cbfda58d8ad4d
Erik Moqvist <erik.moqvist@gmail.com>
2024-07-05 05:37:12 +0200

    Refactoring.

--- Moblin/View/Settings/Scenes/ScenesSettingsView.swift
+++ Moblin/View/Settings/Scenes/ScenesSettingsView.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-struct ScenesSettingsView: View {
+private struct ScenesListView: View {
     @EnvironmentObject var model: Model
 
     var database: Database {
@@ -8,57 +8,63 @@ struct ScenesSettingsView: View {
     }
 
     var body: some View {
-        Form {
-            Section {
-                List {
-                    ForEach(database.scenes) { scene in
-                        NavigationLink(destination: SceneSettingsView(scene: scene)) {
-                            HStack {
-                                DraggableItemPrefixView()
-                                Toggle(scene.name, isOn: Binding(get: {
-                                    scene.enabled
-                                }, set: { value in
-                                    scene.enabled = value
-                                    model.store()
-                                    model.resetSelectedScene()
-                                }))
-                            }
-                        }
-                        .swipeActions(edge: .trailing) {
-                            Button(action: {
-                                database.scenes.removeAll { $0 == scene }
+        Section {
+            List {
+                ForEach(database.scenes) { scene in
+                    NavigationLink(destination: SceneSettingsView(scene: scene)) {
+                        HStack {
+                            DraggableItemPrefixView()
+                            Toggle(scene.name, isOn: Binding(get: {
+                                scene.enabled
+                            }, set: { value in
+                                scene.enabled = value
                                 model.store()
                                 model.resetSelectedScene()
-                            }, label: {
-                                Text("Delete")
-                            })
-                            .tint(.red)
-                        }
-                        .swipeActions(edge: .trailing) {
-                            Button(action: {
-                                database.scenes.append(scene.clone())
-                                model.store()
-                                model.resetSelectedScene()
-                            }, label: {
-                                Text("Duplicate")
-                            })
-                            .tint(.blue)
+                            }))
                         }
                     }
-                    .onMove(perform: { froms, to in
-                        database.scenes.move(fromOffsets: froms, toOffset: to)
-                        model.store()
-                        model.resetSelectedScene()
-                    })
+                    .swipeActions(edge: .trailing) {
+                        Button(action: {
+                            database.scenes.removeAll { $0 == scene }
+                            model.store()
+                            model.resetSelectedScene()
+                        }, label: {
+                            Text("Delete")
+                        })
+                        .tint(.red)
+                    }
+                    .swipeActions(edge: .trailing) {
+                        Button(action: {
+                            database.scenes.append(scene.clone())
+                            model.store()
+                            model.resetSelectedScene()
+                        }, label: {
+                            Text("Duplicate")
+                        })
+                        .tint(.blue)
+                    }
                 }
-                CreateButtonView(action: {
-                    database.scenes.append(SettingsScene(name: String(localized: "My scene")))
+                .onMove(perform: { froms, to in
+                    database.scenes.move(fromOffsets: froms, toOffset: to)
                     model.store()
                     model.resetSelectedScene()
                 })
-            } footer: {
-                SwipeLeftToDeleteHelpView(kind: String(localized: "a scene"))
             }
+            CreateButtonView(action: {
+                database.scenes.append(SettingsScene(name: String(localized: "My scene")))
+                model.store()
+                model.resetSelectedScene()
+            })
+        } footer: {
+            SwipeLeftToDeleteHelpView(kind: String(localized: "a scene"))
+        }
+    }
+}
+
+struct ScenesSettingsView: View {
+    var body: some View {
+        Form {
+            ScenesListView()
             Section {
                 NavigationLink(destination: WidgetsSettingsView()) {
                     Text("Widgets")
