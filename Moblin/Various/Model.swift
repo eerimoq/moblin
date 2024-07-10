06c614774cebb17d60245244cc30ba432353a77c
ErAzOr2k <erazor2k@gmail.com>
2024-07-10 21:37:07 +0200

    Remote Control Info and Preview on Watch Continue

--- Moblin/Various/Model.swift
+++ Moblin/Various/Model.swift
@@ -4970,39 +4970,37 @@ extension Model {
     }
 
     func updateRemoteControlAssistantStatus() {
-        guard showingRemoteControl, remoteControlAssistant?.isConnected() == true else {
+        guard showingRemoteControl || isWatchReachable(), remoteControlAssistant?.isConnected() == true else {
             return
         }
         remoteControlAssistant?.getStatus { general, topLeft, topRight in
             self.remoteControlGeneral = general
             self.remoteControlTopLeft = topLeft
             self.remoteControlTopRight = topRight
-            if self.isRemoteControlAssistantConnected() {
-                var thermalState: ProcessInfo.ThermalState
-                switch self.remoteControlGeneral?.flame {
-                case .white:
-                    thermalState = .fair
-                case .yellow:
-                    thermalState = .serious
-                case .red:
-                    thermalState = .critical
-                case .none:
-                    thermalState = .critical
-                }
-                self.sendThermalStateToWatch(thermalState: thermalState)
-                if let recordingMessage = topRight.recording?.message {
-                    self.sendRecordingLengthToWatch(recordingLength: recordingMessage)
-                }
-                if let bitrateMessage = topRight.bitrate?.message {
-                    self.sendSpeedAndTotalToWatch(speedAndTotal: bitrateMessage)
-                }
-                if let audioLevel = topRight.audioLevel {
-                    self.sendAudioLevelToWatch(audioLevel: audioLevel)
-                    self.sendIsMutedToWatch(isMuteOn: audioLevel.isNaN)
-                }
-                self.sendIsLiveToWatch(isLive: topRight.bitrate?.message != nil)
-                self.sendIsRecordingToWatch(isRecording: topRight.recording?.message != nil)
+            var thermalState: ProcessInfo.ThermalState
+            switch self.remoteControlGeneral?.flame {
+            case .white:
+                thermalState = .fair
+            case .yellow:
+                thermalState = .serious
+            case .red:
+                thermalState = .critical
+            case .none:
+                thermalState = .critical
             }
+            self.sendThermalStateToWatch(thermalState: thermalState)
+            if let recordingMessage = topRight.recording?.message {
+                self.sendRecordingLengthToWatch(recordingLength: recordingMessage)
+            }
+            if let bitrateMessage = topRight.bitrate?.message {
+                self.sendSpeedAndTotalToWatch(speedAndTotal: bitrateMessage)
+            }
+            if let audioLevel = topRight.audioLevel {
+                self.sendAudioLevelToWatch(audioLevel: audioLevel)
+                self.sendIsMutedToWatch(isMuteOn: audioLevel.isNaN)
+            }
+            self.sendIsLiveToWatch(isLive: topRight.bitrate?.message != nil)
+            self.sendIsRecordingToWatch(isRecording: topRight.recording?.message != nil)
         }
         remoteControlAssistant?.getSettings { settings in
             self.remoteControlSettings = settings
