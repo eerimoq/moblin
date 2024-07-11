f2e8f53c6691d2a87ef7fa3610cef3bb8eb1839a
Erik Moqvist <erik.moqvist@gmail.com>
2024-07-11 21:17:18 +0200

    QR code widget started.

--- Moblin/VideoEffects/QrCodeEffect.swift
+++ Moblin/VideoEffects/QrCodeEffect.swift
@@ -7,7 +7,9 @@ private let qrCodeQueue = DispatchQueue(label: "com.eerimoq.widget.qr-code")
 final class QrCodeEffect: VideoEffect {
     private let filter = CIFilter.sourceOverCompositing()
     private let widget: SettingsWidgetQrCode
+    private var sceneWidget: SettingsSceneWidget?
     private var image: CIImage?
+    private var imageMetalPetal: MTIImage?
 
     init(widget: SettingsWidgetQrCode) {
         self.widget = widget
@@ -38,8 +40,17 @@ final class QrCodeEffect: VideoEffect {
             .transformed(by: CGAffineTransform(scaleX: scale, y: scale))
             .transformed(by: CGAffineTransform(translationX: x, y: y))
             .cropped(to: .init(x: 0, y: 0, width: size.width, height: size.height))
+        // guard let cgImage = outputImage.cgImage else {
+        //     return
+        // }
+        // let imageMetalPetal = MTIImage(cgImage: cgImage, isOpaque: false).resized(
+        //     to: .init(width: toPixels(sceneWidget.width, size.width), height: toPixels(sceneWidget.height, size.height)),
+        //     resizingMode: .aspect
+        // )
         qrCodeQueue.sync {
             self.image = image
+            // self.imageMetalPetal = imageMetalPetal
+            self.sceneWidget = sceneWidget
         }
     }
 
@@ -50,6 +61,23 @@ final class QrCodeEffect: VideoEffect {
     }
 
     override func executeMetalPetal(_ image: MTIImage?, _: [VNFaceObservation]?, _: Bool) -> MTIImage? {
-        return image
+        guard let image, let imageMetalPetal else {
+            return image
+        }
+        let (overlay, sceneWidget) = qrCodeQueue.sync {
+            (self.imageMetalPetal, self.sceneWidget)
+        }
+        guard let overlay, let sceneWidget else {
+            return image
+        }
+        let x = toPixels(sceneWidget.x, image.extent.size.width) + overlay.size.width / 2
+        let y = toPixels(sceneWidget.y, image.extent.size.height) + overlay.size.height / 2
+        let filter = MTIMultilayerCompositingFilter()
+        filter.inputBackgroundImage = image
+        filter.layers = [
+            .init(content: overlay, position: .init(x: x, y: y)),
+            .init(content: overlay, position: .init(x: x, y: y)),
+        ]
+        return filter.outputImage ?? image
     }
 }
