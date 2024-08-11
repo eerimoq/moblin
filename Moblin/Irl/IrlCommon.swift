cc80ba5608e766dfb649a47b3e73b15d104ad292
Erik Moqvist <erik.moqvist@gmail.com>
2024-08-11 11:35:36 +0200

    IRL streaming protocol ideas.

--- Moblin/Irl/IrlCommon.swift
+++ Moblin/Irl/IrlCommon.swift
@@ -8,26 +8,42 @@ import Foundation
 // - Simple.
 // - Fixed latency?
 //
-// Types
-// (1) Video
-// (2) Audio
-// (3) Video format
-// (4) Audio format
-// (5) Ack
+// Segment type        Direction           Has SN
+// ------------------------------------------------
+// (0) Video           Client to Server    yes
+// (1) Audio           Client to Server    yes
+// (2) Video empty     Client to Server    yes
+// (3) Audio empty     Client to Server    yes
+// (4) Video format    Client to Server    yes
+// (5) Audio format    Client to Server    yes
+// (6) Mux             Client to Server    no (contained segments have)
+// (7) Ack             Server to Client    no
 //
-// SN per type.
+// - Use transport layer packet length as segment length. Typically up to 1400 bytes (roughly MTU).
 //
-// First video/audio packet
-// +---------+---+--------+------------------+---------+
-// | 7b type | 1 | 24b SN | 24b total length | Payload |
-// +---------+---+--------+------------------+---------+
+// First video (0), audio (1), video format (4) or audio format (5) segment, first=1, including total length
+// +---------+-------------+--------------+--------+------------------+-------------------------+
+// | 5b type | 2b reserved | 1b first (1) | 24b SN | 24b total length | payload (PTS, DTS, ...) |
+// +---------+-------------+--------------+--------+------------------+-------------------------+
 //
-// Video/audio packet
-// +---------+---+--------+---------+
-// | 7b type | 0 | 24b SN | Payload |
-// +---------+---+--------+---------+
+// Consecutive video (0), audio (1), video format (4) or audio format (5) segment, first=0
+// +---------+-------------+--------------+--------+---------+
+// | 5b type | 2b reserved | 1b first (0) | 24b SN | payload |
+// +---------+-------------+--------------+--------+---------+
 //
-// Ack packet (sent every 50 ms on all connections)
-// +---------+---+--------+---------+---+------------+-----+------------+---------+-- - -
-// | 7b type | 1 | 24b SN | 7b type | - | 16b length | SN1 | SN3 - SN30 | 7b type | ...
-// +---------+---+--------+---------+---+------------+-----+------------+---------+-- - -
+// Video empty (2) or audio empty (3) segment, sent to drop given segment in receiver
+// +---------+-------------+--------+--------+
+// | 5b type | 3b reserved | 24b SN | 4b PTS |
+// +---------+-------------+--------+--------+
+//
+// Mux (6) segment, containing at least two other segments
+// +---------+-------------+------------+------------+------------+------------+
+// | 5b type | 3b reserved | 16b length | segment #1 | 16b length | segment #2 |
+// +---------+-------------+------------+------------+------------+------------+
+//
+// Ack (7) segment, sent every 50 ms on all connections
+// +---------+-------------+----------------+--------+--------+--------+--------+--------+
+// | 5b type | 3b reserved | 8b singles (3) | 24b SN | 24b SN | 24b SN | 24b SN | 24b SN |
+// +---------+-------------+----------------+--------+--------+--------+--------+--------+
+//                                            single   single   single        range
+//
