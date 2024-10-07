c721ffaabf2c5d342a9a093f08f176f4f5c86552
Erik Moqvist <erik.moqvist@gmail.com>
2024-10-07 07:47:37 +0200

    Refactoring.

--- Moblin/View/Stream/DrawOnStreamView.swift
+++ Moblin/View/Stream/DrawOnStreamView.swift
@@ -9,7 +9,67 @@ struct DrawOnStreamLine: Identifiable {
 
 private var drawing = false
 
-struct DrawOnStreamView: View {
+private struct DrawOnStreamCanvasView: View {
+    @EnvironmentObject var model: Model
+
+    var body: some View {
+        HStack {
+            Spacer(minLength: 0)
+            VStack {
+                Spacer(minLength: 0)
+                Canvas { context, size in
+                    for line in model.drawOnStreamLines {
+                        let width = line.width
+                        if line.points.count > 1 {
+                            context.stroke(
+                                drawOnStreamCreatePath(points: line.points),
+                                with: .color(line.color),
+                                lineWidth: width
+                            )
+                        } else {
+                            let point = line.points[0]
+                            var path = Path()
+                            path.addEllipse(in: CGRect(x: point.x, y: point.y, width: 1, height: 1))
+                            context.stroke(path, with: .color(line.color), lineWidth: width)
+                        }
+                    }
+                    model.drawOnStreamSize = size
+                }
+                .gesture(
+                    DragGesture(minimumDistance: 0)
+                        .onChanged { value in
+                            let position = value.location
+                            if value.translation == .zero {
+                                if !drawing {
+                                    model.drawOnStreamLines.append(DrawOnStreamLine(
+                                        points: [position],
+                                        width: model.drawOnStreamSelectedWidth,
+                                        color: model.drawOnStreamSelectedColor
+                                    ))
+                                }
+                                drawing = true
+                            } else {
+                                guard let lastIndex = model.drawOnStreamLines.indices.last else {
+                                    return
+                                }
+                                model.drawOnStreamLines[lastIndex].points.append(position)
+                            }
+                        }
+                        .onEnded { _ in
+                            model.drawOnStreamLineComplete()
+                            drawing = false
+                        }
+                )
+                .aspectRatio(16 / 9, contentMode: .fit)
+                Spacer(minLength: 0)
+            }
+        }
+        .ignoresSafeArea()
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+private struct DrawOnStreamControlsView: View {
     @EnvironmentObject var model: Model
 
     private func buttonColor() -> Color {
@@ -21,93 +81,47 @@ struct DrawOnStreamView: View {
     }
 
     var body: some View {
-        ZStack {
+        VStack {
+            Spacer()
             HStack {
-                Spacer(minLength: 0)
-                VStack {
-                    Spacer(minLength: 0)
-                    Canvas { context, size in
-                        for line in model.drawOnStreamLines {
-                            let width = line.width
-                            if line.points.count > 1 {
-                                context.stroke(
-                                    drawOnStreamCreatePath(points: line.points),
-                                    with: .color(line.color),
-                                    lineWidth: width
-                                )
-                            } else {
-                                let point = line.points[0]
-                                var path = Path()
-                                path.addEllipse(in: CGRect(x: point.x, y: point.y, width: 1, height: 1))
-                                context.stroke(path, with: .color(line.color), lineWidth: width)
-                            }
-                        }
-                        model.drawOnStreamSize = size
-                    }
-                    .gesture(
-                        DragGesture(minimumDistance: 0)
-                            .onChanged { value in
-                                let position = value.location
-                                if value.translation == .zero {
-                                    if !drawing {
-                                        model.drawOnStreamLines.append(DrawOnStreamLine(
-                                            points: [position],
-                                            width: model.drawOnStreamSelectedWidth,
-                                            color: model.drawOnStreamSelectedColor
-                                        ))
-                                    }
-                                    drawing = true
-                                } else {
-                                    guard let lastIndex = model.drawOnStreamLines.indices.last else {
-                                        return
-                                    }
-                                    model.drawOnStreamLines[lastIndex].points.append(position)
-                                }
-                            }
-                            .onEnded { _ in
-                                model.drawOnStreamLineComplete()
-                                drawing = false
-                            }
-                    )
-                    .aspectRatio(16 / 9, contentMode: .fit)
-                    Spacer(minLength: 0)
-                }
-            }
-            .ignoresSafeArea()
-            .edgesIgnoringSafeArea(.all)
-            VStack {
                 Spacer()
                 HStack {
-                    Spacer()
-                    HStack {
-                        Button {
-                            model.drawOnStreamWipe()
-                        } label: {
-                            Image(systemName: "trash.fill")
-                                .font(.title)
-                                .foregroundColor(buttonColor())
-                        }
-                        .disabled(model.drawOnStreamLines.isEmpty)
-                        Button {
-                            model.drawOnStreamUndo()
-                        } label: {
-                            Image(systemName: "arrow.uturn.backward")
-                                .font(.title)
-                                .foregroundColor(buttonColor())
-                        }
-                        .disabled(model.drawOnStreamLines.isEmpty)
-                        ColorPicker("Color", selection: $model.drawOnStreamSelectedColor)
-                            .labelsHidden()
-                        Slider(value: $model.drawOnStreamSelectedWidth, in: 1 ... 20)
-                            .frame(width: 150)
-                            .accentColor(model.drawOnStreamSelectedColor)
+                    Button {
+                        model.drawOnStreamWipe()
+                    } label: {
+                        Image(systemName: "trash.fill")
+                            .font(.title)
+                            .foregroundColor(buttonColor())
                     }
-                    .padding(8)
-                    .background(backgroundColor)
-                    .cornerRadius(5)
+                    .disabled(model.drawOnStreamLines.isEmpty)
+                    Button {
+                        model.drawOnStreamUndo()
+                    } label: {
+                        Image(systemName: "arrow.uturn.backward")
+                            .font(.title)
+                            .foregroundColor(buttonColor())
+                    }
+                    .disabled(model.drawOnStreamLines.isEmpty)
+                    ColorPicker("Color", selection: $model.drawOnStreamSelectedColor)
+                        .labelsHidden()
+                    Slider(value: $model.drawOnStreamSelectedWidth, in: 1 ... 20)
+                        .frame(width: 150)
+                        .accentColor(model.drawOnStreamSelectedColor)
                 }
-                .padding([.trailing], 15)
+                .padding(8)
+                .background(backgroundColor)
+                .cornerRadius(5)
             }
+            .padding([.trailing], 15)
+        }
+    }
+}
+
+struct DrawOnStreamView: View {
+    var body: some View {
+        ZStack {
+            DrawOnStreamCanvasView()
+            DrawOnStreamControlsView()
         }
     }
 }
