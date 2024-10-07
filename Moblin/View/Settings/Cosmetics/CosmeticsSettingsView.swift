d31cb61aa11f6febc9c9063bcb0785cc8ee4d0d9
Erik Moqvist <erik.moqvist@gmail.com>
2024-10-07 07:56:06 +0200

    Refactoring.

--- Moblin/View/Settings/Cosmetics/CosmeticsSettingsView.swift
+++ Moblin/View/Settings/Cosmetics/CosmeticsSettingsView.swift
@@ -1,8 +1,95 @@
 import SwiftUI
 
-struct CosmeticsSettingsView: View {
+private struct CosmeticsSettingsRestoreView: View {
+    @EnvironmentObject var model: Model
+
+    var body: some View {
+        Section {
+            Button {
+                Task {
+                    await model.updateProductFromAppStore()
+                }
+            } label: {
+                HStack {
+                    Spacer()
+                    Text("Restore purchases")
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+private struct CosmeticsSettingsBoughtEverythingView: View {
+    var body: some View {
+        Section {
+            HStack {
+                Text("You already bought everything!")
+                Image(systemName: "heart.fill")
+                    .foregroundColor(.red)
+            }
+        } header: {
+            Text("Icons in store")
+        } footer: {
+            Text("Many thanks from the Moblin developers!")
+        }
+    }
+}
+
+private struct CosmeticsSettingsIconsInStoreView: View {
     @EnvironmentObject var model: Model
     @State var disabledPurchaseButtons: Set<String> = []
+
+    var body: some View {
+        Section {
+            List {
+                ForEach(model.iconsInStore) { icon in
+                    HStack {
+                        Text("")
+                        Image(icon.imageNoBackground())
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: buttonSize, height: buttonSize)
+                        Spacer()
+                        Text(icon.name)
+                        ZStack {
+                            Button(action: {
+                                disabledPurchaseButtons.insert(icon.id)
+                                disabledPurchaseButtons = disabledPurchaseButtons
+                                Task {
+                                    do {
+                                        try await model.purchaseProduct(id: icon.id)
+                                    } catch {
+                                        logger
+                                            .info(
+                                                "cosmetics: Purchase failed with error \(error)"
+                                            )
+                                    }
+                                    disabledPurchaseButtons.remove(icon.id)
+                                    disabledPurchaseButtons = disabledPurchaseButtons
+                                }
+                            }, label: {
+                                Text(icon.price)
+                            })
+                            .padding([.leading], 10)
+                            .disabled(disabledPurchaseButtons.contains(icon.id))
+                            .opacity(disabledPurchaseButtons.contains(icon.id) ? 0.0 : 1.0)
+                            if disabledPurchaseButtons.contains(icon.id) {
+                                ProgressView().padding([.leading], 10)
+                            }
+                        }
+                    }
+                    .tag(icon.image())
+                }
+            }
+        } header: {
+            Text("Icons in store")
+        }
+    }
+}
+
+private struct CosmeticsSettingsMyIconsView: View {
+    @EnvironmentObject var model: Model
 
     private func setAppIcon(iconImage: String) {
         var iconImage: String? = iconImage
@@ -17,6 +104,40 @@ struct CosmeticsSettingsView: View {
     }
 
     var body: some View {
+        Section {
+            Picker("", selection: $model.iconImage) {
+                ForEach(model.myIcons) { icon in
+                    HStack {
+                        Text("")
+                        Image(icon.imageNoBackground())
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: buttonSize, height: buttonSize)
+                        Spacer()
+                        Text(icon.name)
+                    }
+                    .tag(icon.image())
+                }
+            }
+            .onChange(of: model.iconImage) { iconImage in
+                model.database.iconImage = iconImage
+                setAppIcon(iconImage: iconImage)
+            }
+            .pickerStyle(.inline)
+            .labelsHidden()
+        } header: {
+            Text("My icons")
+        } footer: {
+            Text("Displayed in main view and as app icon.")
+        }
+    }
+}
+
+struct CosmeticsSettingsView: View {
+    @EnvironmentObject var model: Model
+    @State var disabledPurchaseButtons: Set<String> = []
+
+    var body: some View {
         Form {
             Section {
                 HStack {
@@ -25,103 +146,13 @@ struct CosmeticsSettingsView: View {
                         .foregroundColor(.red)
                 }
             }
-            Section {
-                Picker("", selection: $model.iconImage) {
-                    ForEach(model.myIcons) { icon in
-                        HStack {
-                            Text("")
-                            Image(icon.imageNoBackground())
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: buttonSize, height: buttonSize)
-                            Spacer()
-                            Text(icon.name)
-                        }
-                        .tag(icon.image())
-                    }
-                }
-                .onChange(of: model.iconImage) { iconImage in
-                    model.database.iconImage = iconImage
-                    setAppIcon(iconImage: iconImage)
-                }
-                .pickerStyle(.inline)
-                .labelsHidden()
-            } header: {
-                Text("My icons")
-            } footer: {
-                Text("Displayed in main view and as app icon.")
-            }
+            CosmeticsSettingsMyIconsView()
             if !model.iconsInStore.isEmpty {
-                Section {
-                    List {
-                        ForEach(model.iconsInStore) { icon in
-                            HStack {
-                                Text("")
-                                Image(icon.imageNoBackground())
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: buttonSize, height: buttonSize)
-                                Spacer()
-                                Text(icon.name)
-                                ZStack {
-                                    Button(action: {
-                                        disabledPurchaseButtons.insert(icon.id)
-                                        disabledPurchaseButtons = disabledPurchaseButtons
-                                        Task {
-                                            do {
-                                                try await model.purchaseProduct(id: icon.id)
-                                            } catch {
-                                                logger
-                                                    .info(
-                                                        "cosmetics: Purchase failed with error \(error)"
-                                                    )
-                                            }
-                                            disabledPurchaseButtons.remove(icon.id)
-                                            disabledPurchaseButtons = disabledPurchaseButtons
-                                        }
-                                    }, label: {
-                                        Text(icon.price)
-                                    })
-                                    .padding([.leading], 10)
-                                    .disabled(disabledPurchaseButtons.contains(icon.id))
-                                    .opacity(disabledPurchaseButtons.contains(icon.id) ? 0.0 : 1.0)
-                                    if disabledPurchaseButtons.contains(icon.id) {
-                                        ProgressView().padding([.leading], 10)
-                                    }
-                                }
-                            }
-                            .tag(icon.image())
-                        }
-                    }
-                } header: {
-                    Text("Icons in store")
-                }
+                CosmeticsSettingsIconsInStoreView()
             } else {
-                Section {
-                    HStack {
-                        Text("You already bought everything!")
-                        Image(systemName: "heart.fill")
-                            .foregroundColor(.red)
-                    }
-                } header: {
-                    Text("Icons in store")
-                } footer: {
-                    Text("Many thanks from the Moblin developers!")
-                }
+                CosmeticsSettingsBoughtEverythingView()
             }
-            Section {
-                Button {
-                    Task {
-                        await model.updateProductFromAppStore()
-                    }
-                } label: {
-                    HStack {
-                        Spacer()
-                        Text("Restore purchases")
-                        Spacer()
-                    }
-                }
-            }
+            CosmeticsSettingsRestoreView()
         }
         .navigationTitle("Cosmetics")
     }
