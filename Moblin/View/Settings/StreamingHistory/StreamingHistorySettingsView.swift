203d08538ce2a4fb2374b9a76a24ce9d6b48aeae
Erik Moqvist <erik.moqvist@gmail.com>
2024-10-08 07:29:25 +0200

    Refactoring.

--- Moblin/View/Settings/StreamingHistory/StreamingHistorySettingsView.swift
+++ Moblin/View/Settings/StreamingHistory/StreamingHistorySettingsView.swift
@@ -1,72 +1,86 @@
 import SwiftUI
 
-struct StreamingHistorySettingsView: View {
+private struct StreamingHistorySettingsSummaryView: View {
     @EnvironmentObject var model: Model
 
     private var history: StreamingHistory {
         model.streamingHistory
     }
 
+    var body: some View {
+        HStack {
+            Spacer()
+            VStack {
+                Text("\(history.database.totalStreams!)")
+                    .font(.title2)
+                Text("Total streams")
+                    .font(.subheadline)
+            }
+            Spacer()
+            VStack {
+                Text(history.database.totalTime!.format())
+                    .font(.title2)
+                Text("Total time")
+                    .font(.subheadline)
+            }
+            Spacer()
+            VStack {
+                Text(history.database.totalBytes!.formatBytes())
+                    .font(.title2)
+                Text("Total sent")
+                    .font(.subheadline)
+            }
+            Spacer()
+        }
+    }
+}
+
+private struct StreamingHistorySettingsStreamsView: View {
+    @EnvironmentObject var model: Model
+
     private func formatStreamTitle(stream: StreamingHistoryStream) -> String {
         return "\(stream.startTime.formatted()), \(stream.duration().format())"
     }
 
     var body: some View {
-        VStack {
-            HStack {
-                Spacer()
-                VStack {
-                    Text("\(history.database.totalStreams!)")
-                        .font(.title2)
-                    Text("Total streams")
-                        .font(.subheadline)
-                }
-                Spacer()
-                VStack {
-                    Text(history.database.totalTime!.format())
-                        .font(.title2)
-                    Text("Total time")
-                        .font(.subheadline)
-                }
-                Spacer()
-                VStack {
-                    Text(history.database.totalBytes!.formatBytes())
-                        .font(.title2)
-                    Text("Total sent")
-                        .font(.subheadline)
-                }
-                Spacer()
-            }
-            Form {
-                Section {
-                    List {
-                        ForEach(model.streamingHistory.database.streams) { stream in
-                            NavigationLink {
-                                StreamingHistoryStreamSettingsView(stream: stream)
-                            } label: {
-                                HStack {
-                                    if stream.isSuccessful() {
-                                        Image(systemName: "checkmark.circle")
-                                            .foregroundColor(.green)
-                                    } else {
-                                        Image(systemName: "exclamationmark.circle")
-                                            .foregroundColor(.red)
-                                    }
-                                    VStack(alignment: .leading) {
-                                        Text(formatStreamTitle(stream: stream))
-                                        Text(stream.settings.name)
-                                            .font(.footnote)
-                                    }
+        Form {
+            Section {
+                List {
+                    ForEach(model.streamingHistory.database.streams) { stream in
+                        NavigationLink {
+                            StreamingHistoryStreamSettingsView(stream: stream)
+                        } label: {
+                            HStack {
+                                if stream.isSuccessful() {
+                                    Image(systemName: "checkmark.circle")
+                                        .foregroundColor(.green)
+                                } else {
+                                    Image(systemName: "exclamationmark.circle")
+                                        .foregroundColor(.red)
+                                }
+                                VStack(alignment: .leading) {
+                                    Text(formatStreamTitle(stream: stream))
+                                    Text(stream.settings.name)
+                                        .font(.footnote)
                                 }
                             }
                         }
-                        .onDelete(perform: { offsets in
-                            model.streamingHistory.database.streams.remove(atOffsets: offsets)
-                            model.streamingHistory.store()
-                        })
                     }
+                    .onDelete(perform: { offsets in
+                        model.streamingHistory.database.streams.remove(atOffsets: offsets)
+                        model.streamingHistory.store()
+                    })
                 }
             }
+        }
+    }
+}
+
+struct StreamingHistorySettingsView: View {
+    var body: some View {
+        VStack {
+            StreamingHistorySettingsSummaryView()
+            StreamingHistorySettingsStreamsView()
         }
         .navigationTitle("Streaming history")
     }
--- Moblin/View/Settings/StreamingHistory/StreamingHistoryStreamSettingsView.swift
+++ Moblin/View/Settings/StreamingHistory/StreamingHistoryStreamSettingsView.swift
@@ -1,5 +1,104 @@
 import SwiftUI
 
+private struct StreamingHistoryStreamSettingsGeneralView: View {
+    var stream: StreamingHistoryStream
+
+    var body: some View {
+        Section {
+            TextValueView(name: String(localized: "Start time"), value: stream.startTime.formatted())
+            TextValueView(
+                name: String(localized: "Duration"),
+                value: stream.duration().formatWithSeconds()
+            )
+            TextValueView(
+                name: String(localized: "Total sent"),
+                value: stream.totalBytes.formatBytes()
+            )
+            TextValueView(
+                name: String(localized: "Average bitrate"),
+                value: stream.averageBitrateString()
+            )
+            TextValueView(
+                name: String(localized: "Highest bitrate"),
+                value: stream.highestBitrateString()
+            )
+            HStack {
+                if stream.numberOfFffffs! != 0 {
+                    Image(systemName: "exclamationmark.circle")
+                        .foregroundColor(.red)
+                }
+                TextValueView(name: String(localized: "FFFFF:s"), value: "\(stream.numberOfFffffs!)")
+            }
+            TextValueView(
+                name: String(localized: "Chat messages"),
+                value: stream.numberOfChatMessagesString()
+            )
+        } header: {
+            Text("General")
+        }
+    }
+}
+
+private struct StreamingHistoryStreamSettingsDeviceHealthView: View {
+    var stream: StreamingHistoryStream
+
+    var body: some View {
+        Section {
+            HStack {
+                Text("Highest thermal state")
+                Spacer()
+                Image(systemName: "flame")
+                    .padding([.leading, .trailing], 4)
+                    .padding([.top, .bottom], 2)
+                    .foregroundColor(stream.highestThermalState!.toProcessInfo().color())
+                    .background(.black)
+                    .cornerRadius(3)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 3)
+                            .stroke(.secondary)
+                    )
+            }
+            TextValueView(
+                name: String(localized: "Lowest battery percentage"),
+                value: stream.lowestBatteryPercentageString()
+            )
+        } header: {
+            Text("Device health")
+        }
+    }
+}
+
+private struct StreamingHistoryStreamSettingsSettingsView: View {
+    var stream: StreamingHistoryStream
+
+    var body: some View {
+        Section {
+            TextValueView(name: String(localized: "Name"), value: stream.settings.name)
+            TextValueView(
+                name: String(localized: "Resolution"),
+                value: stream.settings.resolutionString()
+            )
+            TextValueView(name: String(localized: "FPS"), value: "\(stream.settings.fps)")
+            TextValueView(
+                name: String(localized: "Protocol"),
+                value: stream.settings.protocolString()
+            )
+            TextValueView(name: String(localized: "Codec"), value: stream.settings.codecString())
+            TextValueView(name: String(localized: "Bitrate"), value: stream.settings.bitrateString())
+            TextValueView(
+                name: String(localized: "Audio codec"),
+                value: stream.settings.audioCodecString()
+            )
+            TextValueView(
+                name: String(localized: "Audio bitrate"),
+                value: stream.settings.audioBitrateString()
+            )
+        } header: {
+            Text("Settings")
+        }
+    }
+}
+
 struct StreamingHistoryStreamSettingsView: View {
     @EnvironmentObject var model: Model
     var stream: StreamingHistoryStream
@@ -13,84 +112,9 @@ struct StreamingHistoryStreamSettingsView: View {
                 }
             }
             Form {
-                Section {
-                    TextValueView(name: String(localized: "Start time"), value: stream.startTime.formatted())
-                    TextValueView(
-                        name: String(localized: "Duration"),
-                        value: stream.duration().formatWithSeconds()
-                    )
-                    TextValueView(
-                        name: String(localized: "Total sent"),
-                        value: stream.totalBytes.formatBytes()
-                    )
-                    TextValueView(
-                        name: String(localized: "Average bitrate"),
-                        value: stream.averageBitrateString()
-                    )
-                    TextValueView(
-                        name: String(localized: "Highest bitrate"),
-                        value: stream.highestBitrateString()
-                    )
-                    HStack {
-                        if stream.numberOfFffffs! != 0 {
-                            Image(systemName: "exclamationmark.circle")
-                                .foregroundColor(.red)
-                        }
-                        TextValueView(name: String(localized: "FFFFF:s"), value: "\(stream.numberOfFffffs!)")
-                    }
-                    TextValueView(
-                        name: String(localized: "Chat messages"),
-                        value: stream.numberOfChatMessagesString()
-                    )
-                } header: {
-                    Text("General")
-                }
-                Section {
-                    HStack {
-                        Text("Highest thermal state")
-                        Spacer()
-                        Image(systemName: "flame")
-                            .padding([.leading, .trailing], 4)
-                            .padding([.top, .bottom], 2)
-                            .foregroundColor(stream.highestThermalState!.toProcessInfo().color())
-                            .background(.black)
-                            .cornerRadius(3)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 3)
-                                    .stroke(.secondary)
-                            )
-                    }
-                    TextValueView(
-                        name: String(localized: "Lowest battery percentage"),
-                        value: stream.lowestBatteryPercentageString()
-                    )
-                } header: {
-                    Text("Device health")
-                }
-                Section {
-                    TextValueView(name: String(localized: "Name"), value: stream.settings.name)
-                    TextValueView(
-                        name: String(localized: "Resolution"),
-                        value: stream.settings.resolutionString()
-                    )
-                    TextValueView(name: String(localized: "FPS"), value: "\(stream.settings.fps)")
-                    TextValueView(
-                        name: String(localized: "Protocol"),
-                        value: stream.settings.protocolString()
-                    )
-                    TextValueView(name: String(localized: "Codec"), value: stream.settings.codecString())
-                    TextValueView(name: String(localized: "Bitrate"), value: stream.settings.bitrateString())
-                    TextValueView(
-                        name: String(localized: "Audio codec"),
-                        value: stream.settings.audioCodecString()
-                    )
-                    TextValueView(
-                        name: String(localized: "Audio bitrate"),
-                        value: stream.settings.audioBitrateString()
-                    )
-                } header: {
-                    Text("Settings")
-                }
+                StreamingHistoryStreamSettingsGeneralView(stream: stream)
+                StreamingHistoryStreamSettingsDeviceHealthView(stream: stream)
+                StreamingHistoryStreamSettingsSettingsView(stream: stream)
             }
         }
         .navigationTitle("Stream summary")
