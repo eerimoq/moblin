da715217ccb3414b99b36ad5cdee503687c688d0
Erik Moqvist <erik.moqvist@gmail.com>
2025-01-22 20:14:03 +0100

    Try fixing LLB crash on iPhone 11.

--- Moblin/Various/Utils.swift
+++ Moblin/Various/Utils.swift
@@ -162,8 +162,7 @@ extension AVCaptureDevice {
     }
 
     func setFps(frameRate: Float64) {
-        // Assigning isAutoVideoFrameRateEnabled makes iPad with USB cam crash. Reading seems fine.
-        if #available(iOS 18, *), isAutoVideoFrameRateEnabled {
+        if #available(iOS 18, *), activeFormat.isAutoVideoFrameRateSupported {
             isAutoVideoFrameRateEnabled = false
         }
         activeVideoMinFrameDuration = CMTime(value: 100, timescale: CMTimeScale(100 * frameRate))
