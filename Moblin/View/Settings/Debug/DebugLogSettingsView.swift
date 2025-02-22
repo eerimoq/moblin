425680765858183eb187cf7f24ed9c2804913637
Erik Moqvist <erik.moqvist@gmail.com>
2025-02-22 12:51:03 +0100

    Make debug log buttons easier to hit.

--- Moblin/View/Settings/Debug/DebugLogSettingsView.swift
+++ Moblin/View/Settings/Debug/DebugLogSettingsView.swift
@@ -2,21 +2,25 @@ import Collections
 import SwiftUI
 
 struct DebugLogSettingsView: View {
+    @EnvironmentObject var model: Model
     var log: Deque<LogEntry>
     var formatLog: () -> String
     var clearLog: () -> Void
 
     var body: some View {
         VStack {
-            HStack {
+            HStack(spacing: 15) {
                 Spacer()
                 ShareLink(item: formatLog())
                 Button(action: {
                     clearLog()
+                    model.objectWillChange.send()
                 }, label: {
                     Image(systemName: "trash")
                 })
             }
+            .padding([.top], 10)
+            .padding([.trailing], 15)
             ScrollView {
                 if log.isEmpty {
                     Text("The log is empty.")
