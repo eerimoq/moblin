e272ec8712807081ef9ee465c33efa3c63a024ba
lahari <gplahari@gmail.com>
2025-03-05 00:14:02 +0100

    Don't change scene on scene manipulation that is not deletion of current scene

--- Moblin/View/Settings/Scenes/ScenesSettingsView.swift
+++ Moblin/View/Settings/Scenes/ScenesSettingsView.swift
@@ -20,7 +20,7 @@ private struct ScenesListView: View {
                                 scene.enabled
                             }, set: { value in
                                 scene.enabled = value
-                                model.resetSelectedScene()
+                                model.resetSelectedScene(changeScene: false)
                             }))
                         }
                     }
@@ -36,7 +36,7 @@ private struct ScenesListView: View {
                     .swipeActions(edge: .trailing) {
                         Button(action: {
                             database.scenes.append(scene.clone())
-                            model.resetSelectedScene()
+                            model.resetSelectedScene(changeScene: false)
                         }, label: {
                             Text("Duplicate")
                         })
@@ -45,12 +45,12 @@ private struct ScenesListView: View {
                 }
                 .onMove(perform: { froms, to in
                     database.scenes.move(fromOffsets: froms, toOffset: to)
-                    model.resetSelectedScene()
+                    model.resetSelectedScene(changeScene: false)
                 })
             }
             CreateButtonView {
                 database.scenes.append(SettingsScene(name: String(localized: "My scene")))
-                model.resetSelectedScene()
+                model.resetSelectedScene(changeScene: false)
             }
         } header: {
             Text("Scenes")
