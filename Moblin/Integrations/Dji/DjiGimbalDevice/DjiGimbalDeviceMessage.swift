1924641a5e42078d155dfa6715c46cdafe488987
Erik Moqvist <erik.moqvist@gmail.com>
2025-03-16 18:24:08 +0100

    DJI gimbal continued.

--- Moblin/Integrations/Dji/DjiGimbalDevice/DjiGimbalDeviceMessage.swift
+++ Moblin/Integrations/Dji/DjiGimbalDevice/DjiGimbalDeviceMessage.swift
@@ -1,19 +1,19 @@
 import Foundation
 
-class DjiGimbalZoomMessagePayload {
+struct DjiGimbalZoomMessagePayload {
     // periphery:ignore
     let maybeZoom: Int16
     // periphery:ignore
     let orThisZoom: Int16
 
     init?(data: Data) {
+        // Three in a row with 0 zoom makes it a button press?
         let reader = ByteArray(data: data)
         do {
             _ = try reader.readBytes(4)
             maybeZoom = try Int16(bitPattern: reader.readUInt16Le())
             _ = try reader.readBytes(3)
             orThisZoom = try Int16(bitPattern: reader.readUInt16Le())
-            _ = try reader.readBytes(2)
         } catch {
             return nil
         }
@@ -27,7 +27,7 @@ enum DjiGimbalTriggerButtonPress: UInt8 {
     case long = 7
 }
 
-class DjiGimbalButtonsMessagePayload {
+struct DjiGimbalButtonsMessagePayload {
     let trigger: DjiGimbalTriggerButtonPress?
     let switchScene: Bool
     let record: Bool
