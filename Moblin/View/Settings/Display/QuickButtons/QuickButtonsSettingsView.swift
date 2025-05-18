7b1b172c8e529c7926cfb6d34b8d0e016e3ccd7b
Erik Moqvist <erik.moqvist@gmail.com>
2025-05-18 09:50:31 +0200

    Refactoring.

--- Moblin/View/Settings/Display/QuickButtons/QuickButtonsSettingsView.swift
+++ Moblin/View/Settings/Display/QuickButtons/QuickButtonsSettingsView.swift
@@ -1,67 +1,55 @@
 import SwiftUI
 
-struct QuickButtonsSettingsView: View {
+private struct AppearenceSettingsView: View {
     @EnvironmentObject var model: Model
 
     var body: some View {
-        Form {
-            Section {
-                if model.database.showAllSettings {
-                    Toggle("Scroll", isOn: Binding(get: {
-                        model.database.quickButtons.enableScroll
-                    }, set: { value in
-                        model.database.quickButtons.enableScroll = value
-                        model.updateQuickButtonStates()
-                        model.scrollQuickButtonsToBottom()
-                    }))
-                    Toggle("Two columns", isOn: Binding(get: {
-                        model.database.quickButtons.twoColumns
-                    }, set: { value in
-                        model.database.quickButtons.twoColumns = value
-                        model.updateQuickButtonStates()
-                        model.scrollQuickButtonsToBottom()
-                    }))
-                }
-                Toggle("Show name", isOn: Binding(get: {
-                    model.database.quickButtons.showName
+        Section {
+            if model.database.showAllSettings {
+                Toggle("Scroll", isOn: Binding(get: {
+                    model.database.quickButtons.enableScroll
                 }, set: { value in
-                    model.database.quickButtons.showName = value
+                    model.database.quickButtons.enableScroll = value
                     model.updateQuickButtonStates()
                     model.scrollQuickButtonsToBottom()
                 }))
-            } header: {
-                Text("Appearence")
-            } footer: {
-                Text("Names are not shown in portrait mode.")
+                Toggle("Two columns", isOn: Binding(get: {
+                    model.database.quickButtons.twoColumns
+                }, set: { value in
+                    model.database.quickButtons.twoColumns = value
+                    model.updateQuickButtonStates()
+                    model.scrollQuickButtonsToBottom()
+                }))
             }
-            Section {
-                List {
-                    ForEach(model.database.globalButtons) { button in
-                        if model.database.showAllSettings {
-                            NavigationLink {
-                                QuickButtonsButtonSettingsView(
-                                    button: button,
-                                    shortcut: false
-                                )
-                            } label: {
-                                Toggle(isOn: Binding(get: {
-                                    button.enabled
-                                }, set: { value in
-                                    button.enabled = value
-                                    model.updateQuickButtonStates()
-                                })) {
-                                    HStack {
-                                        DraggableItemPrefixView()
-                                        IconAndTextView(
-                                            image: button.systemImageNameOff,
-                                            text: button.name,
-                                            longDivider: true
-                                        )
-                                        Spacer()
-                                    }
-                                }
-                            }
-                        } else {
+            Toggle("Show name", isOn: Binding(get: {
+                model.database.quickButtons.showName
+            }, set: { value in
+                model.database.quickButtons.showName = value
+                model.updateQuickButtonStates()
+                model.scrollQuickButtonsToBottom()
+            }))
+        } header: {
+            Text("Appearence")
+        } footer: {
+            Text("Names are not shown in portrait mode.")
+        }
+    }
+}
+
+private struct ButtonsSettingsView: View {
+    @EnvironmentObject var model: Model
+
+    var body: some View {
+        Section {
+            List {
+                ForEach(model.database.globalButtons) { button in
+                    if model.database.showAllSettings {
+                        NavigationLink {
+                            QuickButtonsButtonSettingsView(
+                                button: button,
+                                shortcut: false
+                            )
+                        } label: {
                             Toggle(isOn: Binding(get: {
                                 button.enabled
                             }, set: { value in
@@ -79,16 +67,42 @@ struct QuickButtonsSettingsView: View {
                                 }
                             }
                         }
+                    } else {
+                        Toggle(isOn: Binding(get: {
+                            button.enabled
+                        }, set: { value in
+                            button.enabled = value
+                            model.updateQuickButtonStates()
+                        })) {
+                            HStack {
+                                DraggableItemPrefixView()
+                                IconAndTextView(
+                                    image: button.systemImageNameOff,
+                                    text: button.name,
+                                    longDivider: true
+                                )
+                                Spacer()
+                            }
+                        }
                     }
-                    .onMove(perform: { froms, to in
-                        model.database.globalButtons.move(fromOffsets: froms, toOffset: to)
-                        model.updateQuickButtonStates()
-                        model.sceneUpdated(updateRemoteScene: false)
-                    })
                 }
-            } header: {
-                Text("Quick buttons")
+                .onMove(perform: { froms, to in
+                    model.database.globalButtons.move(fromOffsets: froms, toOffset: to)
+                    model.updateQuickButtonStates()
+                    model.sceneUpdated(updateRemoteScene: false)
+                })
             }
+        } header: {
+            Text("Quick buttons")
+        }
+    }
+}
+
+struct QuickButtonsSettingsView: View {
+    var body: some View {
+        Form {
+            AppearenceSettingsView()
+            ButtonsSettingsView()
         }
         .navigationTitle("Quick buttons")
     }
