f56a1f984dbdcd00a1baadcc01d34d6d3b3fb225
Erik Moqvist <erik.moqvist@gmail.com>
2025-05-18 21:42:13 +0200

    Fix auto scene switcher settings.

--- Moblin/Various/Settings.swift
+++ Moblin/Various/Settings.swift
@@ -835,8 +835,8 @@ class SettingsAutoSceneSwitchers: Codable, Identifiable, ObservableObject {
 
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(.switcherId, switcherId)
         try container.encode(.switchers, switchers)
-        try container.encode(.switchers, switcherId)
     }
 
     init() {}
