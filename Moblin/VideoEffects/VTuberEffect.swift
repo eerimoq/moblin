27568107aa3dc0c17500f11dee67deabcfbbe2e3
Erik Moqvist <erik.moqvist@gmail.com>
2025-05-25 19:52:19 +0200

    VTuber continued.

--- Moblin/VideoEffects/VTuberEffect.swift
+++ Moblin/VideoEffects/VTuberEffect.swift
@@ -10,26 +10,24 @@ final class VTuberEffect: VideoEffect {
     init(vrm: URL) {
         do {
             scene = try VRMSceneLoader(withURL: vrm).loadScene()
-            let cameraNode = SCNNode()
-            cameraNode.camera = SCNCamera()
-            scene!.rootNode.addChildNode(cameraNode)
-            cameraNode.position = SCNVector3(0, 0.8, -1.8)
-            cameraNode.rotation = SCNVector4(0, 1, 0, Float.pi)
-            renderer.scene = scene!
-            let node = scene!.vrmNode
-            // node.setBlendShape(value: 1, for: .preset(.fun))
-            node.humanoid.node(for: .neck)?.eulerAngles = SCNVector3(0, 0, 20 * CGFloat.pi / 180)
-            node.humanoid.node(for: .leftShoulder)?.eulerAngles = SCNVector3(0, 0, 40 * CGFloat.pi / 180)
-            node.humanoid.node(for: .rightShoulder)?.eulerAngles = SCNVector3(0, 0, 40 * CGFloat.pi / 180)
-            // node.runAction(SCNAction.rotateBy(x: -0.5, y: 0, z: 0, duration: 1.0))
-            node.runAction(.repeatForever(.sequence([
-                SCNAction.rotateBy(x: 0, y: -0.5, z: 0, duration: 0.5),
-                SCNAction.rotateBy(x: 0, y: 0.5, z: 0, duration: 0.5),
-            ])))
         } catch {
             logger.info("v-tuber: Failed to load VRM file with error: \(error)")
             scene = nil
         }
+        guard let scene else {
+            return
+        }
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        scene.rootNode.addChildNode(cameraNode)
+        cameraNode.position = SCNVector3(0, 0.8, -1.8)
+        cameraNode.rotation = SCNVector4(0, 1, 0, Float.pi)
+        renderer.scene = scene
+        let node = scene.vrmNode
+        node.runAction(.repeatForever(.sequence([
+            .rotateBy(x: 0, y: -0.5, z: 0, duration: 0.5),
+            .rotateBy(x: 0, y: 0.5, z: 0, duration: 0.5),
+        ])))
     }
 
     override func getName() -> String {
@@ -45,7 +43,19 @@ final class VTuberEffect: VideoEffect {
             return image
         }
         let time = presentationTimeStamp - firstPresentationTimeStamp
-        scene.vrmNode.update(at: time)
+        let node = scene.vrmNode
+        var angle = time.remainder(dividingBy: .pi * 2)
+        if angle < 0 {
+            angle *= -1
+        }
+        angle -= .pi / 2
+        angle *= 0.5
+        node.setBlendShape(value: abs(time.truncatingRemainder(dividingBy: 2) - 1), for: .preset(.blink))
+        node.setBlendShape(value: abs(time.truncatingRemainder(dividingBy: 2) - 1), for: .preset(.angry))
+        node.humanoid.node(for: .leftShoulder)?.eulerAngles = SCNVector3(0, 0, angle)
+        node.humanoid.node(for: .rightShoulder)?.eulerAngles = SCNVector3(0, 0, angle)
+        node.humanoid.node(for: .neck)?.eulerAngles = SCNVector3(0, 0, angle * 0.7)
+        node.update(at: time)
         let vTuberImage = renderer.snapshot(atTime: time,
                                             with: CGSize(width: 300, height: 600),
                                             antialiasingMode: .multisampling4X)
