391141d861ccabdfe1a5c9716553aa2c0f32581a
Erik Moqvist <erik.moqvist@gmail.com>
2025-05-25 15:54:52 +0200

    VTuber started.

---
+++ Moblin/VideoEffects/VTuberEffect.swift
@@ -0,0 +1,21 @@
+import UIKit
+import VRMSceneKit
+
+final class VTuberEffect: VideoEffect {
+    init(vrm: URL) {
+        do {
+            let scene = try VRMSceneLoader(withURL: vrm).loadScene()
+            logger.info("v-tuber: Scene \(scene)")
+        } catch {
+            logger.info("v-tuber: Failed to load VRM file with error: \(error)")
+        }
+    }
+    
+    override func getName() -> String {
+        return "VTuber"
+    }
+
+    override func execute(_ image: CIImage, _: VideoEffectInfo) -> CIImage {
+        return image
+    }
+}
