9d2093d69da7b0265359c9be98e3b961e9129554
Erik Moqvist <erik.moqvist@gmail.com>
2025-06-07 20:22:42 +0200

    Shape effect on image widget. Not perfect, but will do for now.

--- Moblin/VideoEffects/ImageEffect.swift
+++ Moblin/VideoEffects/ImageEffect.swift
@@ -1,29 +1,41 @@
 import AVFoundation
-import MetalPetal
 import UIKit
 import Vision
 
-final class ImageEffect: VideoEffect {
-    private let filter = CIFilter.sourceOverCompositing()
-    private var extent = CGRect.zero {
-        didSet {
-            guard extent != oldValue else {
-                return
-            }
-            prepare(size: extent.size)
-            prepareMetalPetal(size: extent.size)
+extension CGImagePropertyOrientation {
+    init(_ uiOrientation: UIImage.Orientation) {
+        switch uiOrientation {
+        case .up:
+            self = .up
+        case .upMirrored:
+            self = .upMirrored
+        case .down:
+            self = .down
+        case .downMirrored:
+            self = .downMirrored
+        case .left:
+            self = .left
+        case .leftMirrored:
+            self = .leftMirrored
+        case .right:
+            self = .right
+        case .rightMirrored:
+            self = .rightMirrored
+        @unknown default:
+            self = .up
         }
     }
+}
 
-    private var overlay: CIImage?
-    private var overlayMetalPetal: MTIImage?
-    private let originalImage: UIImage
+final class ImageEffect: VideoEffect {
+    private let filter = CIFilter.sourceOverCompositing()
+    private let originalImage: CIImage?
     private var sceneWidget: SettingsSceneWidget?
     private let settingName: String
     let widgetId: UUID
 
     init(image: UIImage, settingName: String, widgetId: UUID) {
-        originalImage = image
+        originalImage = CIImage(image: image)?.oriented(CGImagePropertyOrientation(image.imageOrientation))
         self.settingName = settingName
         self.widgetId = widgetId
         super.init()
@@ -31,11 +43,7 @@ final class ImageEffect: VideoEffect {
 
     func setSceneWidget(sceneWidget: SettingsSceneWidget) {
         mixerLockQueue.async {
-            if self.sceneWidget?.isSamePositioning(other: sceneWidget) != true {
-                self.sceneWidget = sceneWidget
-                self.prepare(size: self.extent.size)
-                self.prepareMetalPetal(size: self.extent.size)
-            }
+            self.sceneWidget = sceneWidget
         }
     }
 
@@ -44,64 +52,13 @@ final class ImageEffect: VideoEffect {
     }
 
     override func execute(_ image: CIImage, _ info: VideoEffectInfo) -> CIImage {
-        extent = image.extent
-        if let overlay {
-            filter.inputImage = applyEffects(overlay, info)
-        } else {
-            filter.inputImage = nil
+        guard let originalImage, let sceneWidget else {
+            return image
         }
+        filter.inputImage = applyEffects(originalImage, info)
+            .resizeMoveMirror(sceneWidget, image.extent.size, false)
+            .cropped(to: image.extent)
         filter.backgroundImage = image
         return filter.outputImage ?? image
-    }
-
-    override func executeMetalPetal(_ image: MTIImage?, _: VideoEffectInfo) -> MTIImage? {
-        guard let image, let sceneWidget else {
-            return image
-        }
-        extent = image.extent
-        guard let overlayMetalPetal else {
-            return image
-        }
-        let x = toPixels(sceneWidget.x, extent.size.width) + overlayMetalPetal.size.width / 2
-        let y = toPixels(sceneWidget.y, extent.size.height) + overlayMetalPetal.size.height / 2
-        let filter = MTIMultilayerCompositingFilter()
-        filter.inputBackgroundImage = image
-        filter.layers = [
-            .init(content: overlayMetalPetal, position: .init(x: x, y: y)),
-        ]
-        return filter.outputImage ?? image
-    }
-
-    private func prepare(size: CGSize) {
-        guard let sceneWidget, size != .zero else {
-            return
-        }
-        UIGraphicsBeginImageContext(size)
-        let x = toPixels(sceneWidget.x, size.width)
-        let y = toPixels(sceneWidget.y, size.height)
-        let width = toPixels(sceneWidget.width, size.width)
-        let height = toPixels(sceneWidget.height, size.height)
-        let image = originalImage.scalePreservingAspectRatio(targetSize: CGSize(
-            width: width,
-            height: height
-        ))
-        image.draw(at: CGPoint(x: x, y: y))
-        overlay = CIImage(
-            image: UIGraphicsGetImageFromCurrentImageContext()!,
-            options: nil
-        )
-        UIGraphicsEndImageContext()
-    }
-
-    private func prepareMetalPetal(size: CGSize) {
-        guard let originalImage = originalImage.cgImage, let sceneWidget, size != .zero else {
-            return
-        }
-        let width = toPixels(sceneWidget.width, size.width)
-        let height = toPixels(sceneWidget.height, size.height)
-        overlayMetalPetal = MTIImage(cgImage: originalImage, isOpaque: true).resized(
-            to: .init(width: width, height: height),
-            resizingMode: .aspect
-        )
     }
 }
