64401967f9a0d0719879793dcad778df6cdd0e6b
Erik Moqvist <erik.moqvist@gmail.com>
2025-07-17 12:23:22 +0200

    HEVC config nal units refactoring.

--- Moblin/Media/HaishinKit/Mpeg/Hevc/MpegTsVideoConfigHevc.swift
+++ Moblin/Media/HaishinKit/Mpeg/Hevc/MpegTsVideoConfigHevc.swift
@@ -10,37 +10,6 @@ struct MpegTsVideoConfigHevc {
     }
 
     var configurationVersion: UInt8 = 1
-    // periphery:ignore
-    var generalProfileSpace: UInt8 = 0
-    var generalTierFlag = false
-    // periphery:ignore
-    var generalProfileIdc: UInt8 = 0
-    // periphery:ignore
-    var generalProfileCompatibilityFlags: UInt32 = 0
-    // periphery:ignore
-    var generalConstraintIndicatorFlags: UInt64 = 0
-    // periphery:ignore
-    var generalLevelIdc: UInt8 = 0
-    // periphery:ignore
-    var minSpatialSegmentationIdc: UInt16 = 0
-    // periphery:ignore
-    var parallelismType: UInt8 = 0
-    // periphery:ignore
-    var chromaFormat: UInt8 = 0
-    // periphery:ignore
-    var bitDepthLumaMinus8: UInt8 = 0
-    // periphery:ignore
-    var bitDepthChromaMinus8: UInt8 = 0
-    // periphery:ignore
-    var avgFrameRate: UInt16 = 0
-    // periphery:ignore
-    var constantFrameRate: UInt8 = 0
-    // periphery:ignore
-    var numTemporalLayers: UInt8 = 0
-    // periphery:ignore
-    var temporalIdNested: UInt8 = 0
-    // periphery:ignore
-    var lengthSizeMinusOne: UInt8 = 0
     private(set) var nalUnits: [HevcNalUnitType: Data] = [:]
 
     init(hvcC: Data) {
@@ -92,41 +61,23 @@ struct MpegTsVideoConfigHevc {
 
     var hvcC: Data {
         get {
-            let buffer = ByteWriter()
-                .writeUInt8(configurationVersion)
-            return buffer.data
+            let writer = ByteWriter()
+            writer.writeUInt8(configurationVersion)
+            return writer.data
         }
         set {
-            let buffer = ByteReader(data: newValue)
+            let reader = ByteReader(data: newValue)
             do {
-                configurationVersion = try buffer.readUInt8()
-                let a = try buffer.readUInt8()
-                generalProfileSpace = a >> 6
-                generalTierFlag = a & 0x20 > 0
-                generalProfileIdc = a & 0x1F
-                generalProfileCompatibilityFlags = try buffer.readUInt32()
-                generalConstraintIndicatorFlags = try UInt64(buffer.readUInt32()) << 16 |
-                    UInt64(buffer.readUInt16())
-                generalLevelIdc = try buffer.readUInt8()
-                minSpatialSegmentationIdc = try buffer.readUInt16() & 0xFFF
-                parallelismType = try buffer.readUInt8() & 0x3
-                chromaFormat = try buffer.readUInt8() & 0x3
-                bitDepthLumaMinus8 = try buffer.readUInt8() & 0x7
-                bitDepthChromaMinus8 = try buffer.readUInt8() & 0x7
-                avgFrameRate = try buffer.readUInt16()
-                let b = try buffer.readUInt8()
-                constantFrameRate = b >> 6
-                numTemporalLayers = b & 0x38 >> 3
-                temporalIdNested = b & 0x6 >> 1
-                lengthSizeMinusOne = b & 0x3
-                let numberOfArrays = try buffer.readUInt8()
+                configurationVersion = try reader.readUInt8()
+                _ = try reader.readBytes(21)
+                let numberOfArrays = try reader.readUInt8()
                 for _ in 0 ..< numberOfArrays {
-                    let a = try buffer.readUInt8()
-                    let nalUnitType = HevcNalUnitType(rawValue: a & 0b0011_1111) ?? .unspec
-                    let numNalus = try buffer.readUInt16()
+                    let header = try reader.readUInt8()
+                    let nalUnitType = HevcNalUnitType(rawValue: header & 0b0011_1111) ?? .unspec
+                    let numNalus = try reader.readUInt16()
                     for _ in 0 ..< numNalus {
-                        let length = try buffer.readUInt16()
-                        try nalUnits[nalUnitType] = buffer.readBytes(Int(length))
+                        let length = try reader.readUInt16()
+                        try nalUnits[nalUnitType] = reader.readBytes(Int(length))
                     }
                 }
             } catch {
