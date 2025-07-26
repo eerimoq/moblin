18a3b6283b1e5b7d07e9e55123a70dbc59ad9db8
Erik Moqvist <erik.moqvist@gmail.com>
2025-07-26 13:44:25 +0200

    Bookmarks in web browser.

--- Moblin/View/WebBrowser/WebBrowserView.swift
+++ Moblin/View/WebBrowser/WebBrowserView.swift
@@ -102,6 +102,8 @@ private struct BookmarksView: View {
                     }
                 } header: {
                     Text("Bookmarks")
+                } footer: {
+                    SwipeLeftToDeleteHelpView(kind: "a bookmark")
                 }
                 Section {
                     Button {
