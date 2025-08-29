1a275c9bc6e1d31dca176dcbe4316734a9d45144
Erik Moqvist <erik.moqvist@gmail.com>
2025-08-29 19:44:24 +0200

    Refactoring.

--- Moblin/Media/HaishinKit/Codec/Audio/AudioEncoderSettings.swift
+++ Moblin/Media/HaishinKit/Codec/Audio/AudioEncoderSettings.swift
@@ -17,40 +17,35 @@ struct AudioEncoderSettings {
         }
 
         func makeAudioFormat(_ inSourceFormat: AudioStreamBasicDescription) -> AVAudioFormat? {
+            let channels = min(inSourceFormat.mChannelsPerFrame, AudioEncoderSettings.maximumNumberOfChannels)
+            var streamDescription: AudioStreamBasicDescription
             switch self {
             case .aac:
-                var streamDescription = AudioStreamBasicDescription(
+                streamDescription = AudioStreamBasicDescription(
                     mSampleRate: inSourceFormat.mSampleRate,
                     mFormatID: kAudioFormatMPEG4AAC,
                     mFormatFlags: UInt32(MPEG4ObjectID.AAC_LC.rawValue),
                     mBytesPerPacket: 0,
                     mFramesPerPacket: 1024,
                     mBytesPerFrame: 0,
-                    mChannelsPerFrame: min(
-                        inSourceFormat.mChannelsPerFrame,
-                        AudioEncoderSettings.maximumNumberOfChannels
-                    ),
+                    mChannelsPerFrame: channels,
                     mBitsPerChannel: 0,
                     mReserved: 0
                 )
-                return AVAudioFormat(streamDescription: &streamDescription)
             case .opus:
-                var streamDescription = AudioStreamBasicDescription(
+                streamDescription = AudioStreamBasicDescription(
                     mSampleRate: inSourceFormat.mSampleRate,
                     mFormatID: kAudioFormatOpus,
                     mFormatFlags: 0,
                     mBytesPerPacket: 0,
                     mFramesPerPacket: 960,
                     mBytesPerFrame: 0,
-                    mChannelsPerFrame: min(
-                        inSourceFormat.mChannelsPerFrame,
-                        AudioEncoderSettings.maximumNumberOfChannels
-                    ),
+                    mChannelsPerFrame: channels,
                     mBitsPerChannel: 0,
                     mReserved: 0
                 )
-                return AVAudioFormat(streamDescription: &streamDescription)
             }
+            return AVAudioFormat(streamDescription: &streamDescription)
         }
     }
 
