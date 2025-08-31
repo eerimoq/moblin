50ba857302ba31675f68a556d0e1d7ebd7803e2b
Erik Moqvist <erik.moqvist@gmail.com>
2025-08-31 13:47:52 +0200

    Camera level color and style.

--- Moblin/View/Stream/CameraLevelView.swift
+++ Moblin/View/Stream/CameraLevelView.swift
@@ -10,39 +10,34 @@ struct CameraLevelView: View {
                 return
             }
             let y = size.height / 2
-            let xLeft: Double
-            let xRight: Double
-            if size.width > size.height {
-                xLeft = size.width * 0.25
-                xRight = size.width * 0.75
-            } else {
-                xLeft = size.width * 0.15
-                xRight = size.width * 0.85
-            }
+            let xLeft = size.width / 3
+            let xRight = size.width * 2 / 3
+            let halfGap = 2.5
+            let shortLine = 25.0
             var path = Path()
             let color: Color
             if abs(angle) < 0.01 {
-                path.move(to: CGPoint(x: xLeft, y: y))
-                path.addLine(to: CGPoint(x: xRight, y: y))
+                path.move(to: CGPoint(x: xLeft - shortLine - halfGap, y: y))
+                path.addLine(to: CGPoint(x: xRight + shortLine + halfGap, y: y))
                 color = .yellow
             } else {
                 // Left
-                path.move(to: CGPoint(x: xLeft, y: y))
-                path.addLine(to: CGPoint(x: xLeft + 25, y: y))
+                path.move(to: CGPoint(x: xLeft - shortLine - halfGap, y: y))
+                path.addLine(to: CGPoint(x: xLeft - halfGap, y: y))
                 // Right
-                path.move(to: CGPoint(x: xRight - 25, y: y))
-                path.addLine(to: CGPoint(x: xRight, y: y))
+                path.move(to: CGPoint(x: xRight + halfGap, y: y))
+                path.addLine(to: CGPoint(x: xRight + halfGap + shortLine, y: y))
                 // Middle
                 if abs(angle) < 0.5 {
-                    let lineLength = xRight - xLeft - 2 * 30
-                    let xLine = cos(angle) * lineLength / 2
-                    let yLine = sin(angle) * lineLength / 2
+                    let longLine = xRight - xLeft - 2 * halfGap
+                    let xLine = cos(angle) * longLine / 2
+                    let yLine = sin(angle) * longLine / 2
                     path.move(to: CGPoint(x: size.width / 2 - xLine, y: y - yLine))
                     path.addLine(to: CGPoint(x: size.width / 2 + xLine, y: y + yLine))
                 }
-                color = .gray
+                color = .white
             }
-            context.stroke(path, with: .color(color), lineWidth: 1.5)
+            context.stroke(path, with: .color(color), lineWidth: 1)
         }
         .allowsHitTesting(false)
     }
