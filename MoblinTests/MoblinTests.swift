402c8a01264297978f95d223a64763410dcc21c8
Erik Moqvist <erik.moqvist@gmail.com>
2025-09-21 05:55:51 +0200

    Subtitles test.

--- MoblinTests/MoblinTests.swift
+++ MoblinTests/MoblinTests.swift
@@ -1,6 +1,6 @@
-import Testing
-@testable import Moblin
 import AVFoundation
+@testable import Moblin
+import Testing
 
 struct MoblinTests {
     @Test func wrappingTimestamp() async throws {
@@ -17,5 +17,67 @@ struct MoblinTests {
         #expect(timestamp.update(CMTime(seconds: 0)).seconds == 2048)
         #expect(timestamp.update(CMTime(seconds: 1022)).seconds == 2046)
         #expect(timestamp.update(CMTime(seconds: 1022)).seconds == 2046)
+    }
+
+    @Test func subtitlesLines() async throws {
+        let subtitles = Subtitles()
+        var position = 0
+        var frozen = ""
+        var partial = "Hello"
+        subtitles.updateSubtitles(position: position, text: frozen + partial)
+        #expect(subtitles.lines == [
+            "Hello",
+        ])
+        frozen = "Hello What is up "
+        partial = "Not much"
+        subtitles.updateSubtitles(position: position, text: frozen + partial)
+        #expect(subtitles.lines == [
+            "Hello What is up Not much",
+        ])
+        frozen = "Hello What is up Not much at all "
+        partial = """
+        He said will it continue to be a long string when will it go to be the frozen one maybe it will \
+        be the frozen one now no it just continues so the partial one can be really really long
+        """
+        subtitles.updateSubtitles(position: position, text: frozen + partial)
+        #expect(subtitles.lines == [
+            "no it just continues so the partial one can be",
+            "really really long",
+        ])
+        position = 66
+        frozen = """
+        long string when will it go to be the frozen one maybe it will be the frozen one now no it just \
+        continues so the partial one can be really really long
+        """ + " "
+        partial = "Have to wait"
+        subtitles.updateSubtitles(position: position, text: frozen + partial)
+        #expect(subtitles.lines == [
+            "no it just continues so the partial one can be",
+            "really really long Have to wait",
+
+        ])
+        position = 109
+        frozen = """
+        n one maybe it will be the frozen one now no it just continues so the partial one can be really \
+        really long Have to wait a while and maybe it switches
+        """ + " "
+        partial = "Yes"
+        subtitles.updateSubtitles(position: position, text: frozen + partial)
+        #expect(subtitles.lines == [
+            "really really long Have to wait a while and maybe it",
+            "switches Yes",
+        ])
+        partial = "Yes no maybe something is coming up"
+        subtitles.updateSubtitles(position: position, text: frozen + partial)
+        #expect(subtitles.lines == [
+            "really really long Have to wait a while and maybe it",
+            "switches Yes no maybe something is coming up",
+        ])
+        partial = "Yes no maybe something is coming up or is it"
+        subtitles.updateSubtitles(position: position, text: frozen + partial)
+        #expect(subtitles.lines == [
+            "switches Yes no maybe something is coming up or",
+            "is it"
+        ])
     }
 }
