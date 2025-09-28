877913a83145fae2ed2aeb5be5d9c1fe04ef3d7f
Erik Moqvist <erik.moqvist@gmail.com>
2025-09-28 10:05:51 +0200

    Thermal state help.

--- Moblin/View/ControlBar/ThermalStateSheetView.swift
+++ Moblin/View/ControlBar/ThermalStateSheetView.swift
@@ -18,7 +18,7 @@ private struct CloseToolbar: ToolbarContent {
 
 private struct FlameStateView: View {
     let color: Color
-    let text: String
+    let text: LocalizedStringKey
 
     var body: some View {
         HStack {
@@ -32,19 +32,47 @@ private struct FlameStateView: View {
     }
 }
 
+private struct BulletView: View {
+    let text: LocalizedStringKey
+
+    var body: some View {
+        HStack(spacing: 0) {
+            Text(String("• "))
+            Text(text)
+        }
+    }
+}
+
 struct ThermalStateSheetView: View {
     @Binding var presenting: Bool
 
     var body: some View {
         NavigationStack {
             Form {
-                VStack(alignment: .leading) {
-                    FlameStateView(color: .white,
-                                   text: String(localized: "Your device is cold and should function normally."))
-                    FlameStateView(color: .yellow,
-                                   text: String(localized: "Your device is warm, but should function normally."))
-                    FlameStateView(color: .red,
-                                   text: String(localized: "Your device is hot and may overheat."))
+                Section {
+                    VStack(alignment: .leading) {
+                        FlameStateView(color: .white, text: "Your device is cold and should function normally.")
+                        FlameStateView(color: .yellow, text: "Your device is warm, but should function normally.")
+                        FlameStateView(color: .red, text: "Your device is hot and may overheat.")
+                    }
+                }
+                Section {
+                    VStack(alignment: .leading) {
+                        BulletView(text: "Use single lens camera")
+                        BulletView(text: "Lower FPS")
+                        BulletView(text: "Lower resolution")
+                        BulletView(text: "Lower bitrate")
+                        BulletView(text: "No widgets, LUTs or other image effects")
+                        BulletView(text: "More air flow")
+                        BulletView(text: "No direct sunlight")
+                        BulletView(text: "Keep the battery fully charged")
+                        BulletView(text: "Use a phone cooler")
+                        BulletView(text: "Turn off cellular")
+                        BulletView(text: "Turn off busy chats")
+                        BulletView(text: "And a lot more...")
+                    }
+                } header: {
+                    Text("Mitigating overheating")
                 }
             }
             .navigationTitle("Thermal state")
