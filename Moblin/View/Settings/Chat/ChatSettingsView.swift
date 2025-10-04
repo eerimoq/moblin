02309289e10ee37d9f06a23de293593c9cc8bf78
Erik Moqvist <erik.moqvist@gmail.com>
2025-10-04 13:09:04 +0200

    Chat settings redeisgn.

--- Moblin/View/Settings/Chat/ChatSettingsView.swift
+++ Moblin/View/Settings/Chat/ChatSettingsView.swift
@@ -5,36 +5,68 @@ private struct ChatSettingsAppearanceView: View {
     @ObservedObject var chat: SettingsChat
 
     var body: some View {
-        Section {
-            HStack {
-                Text("Font size")
-                Slider(
-                    value: $chat.fontSize,
-                    in: 10 ... 30,
-                    step: 1,
-                    label: {
-                        EmptyView()
-                    },
-                    onEditingChanged: { begin in
-                        guard !begin else {
-                            return
+        NavigationLink {
+            Form {
+                Section {
+                    HStack {
+                        Text("Font size")
+                        Slider(
+                            value: $chat.fontSize,
+                            in: 10 ... 30,
+                            step: 1,
+                            label: {
+                                EmptyView()
+                            },
+                            onEditingChanged: { begin in
+                                guard !begin else {
+                                    return
+                                }
+                                model.reloadChatMessages()
+                            }
+                        )
+                        .onChange(of: chat.fontSize) { _ in
+                            model.reloadChatMessages()
                         }
-                        model.reloadChatMessages()
+                        Text(String(Int(chat.fontSize)))
+                            .frame(width: 25)
                     }
-                )
-                .onChange(of: chat.fontSize) { _ in
-                    model.reloadChatMessages()
-                }
-                Text(String(Int(chat.fontSize)))
-                    .frame(width: 25)
-            }
-            if model.database.showAllSettings {
-                Picker("Display style", selection: $chat.displayStyle) {
-                    ForEach(SettingsChatDisplayStyle.allCases, id: \.self) { displayStyle in
-                        Text(displayStyle.toString())
+                    if model.database.showAllSettings {
+                        Picker("Display style", selection: $chat.displayStyle) {
+                            ForEach(SettingsChatDisplayStyle.allCases, id: \.self) { displayStyle in
+                                Text(displayStyle.toString())
+                            }
+                        }
+                        Toggle("Timestamp", isOn: $chat.timestampColorEnabled)
+                            .onChange(of: chat.timestampColorEnabled) { _ in
+                                model.reloadChatMessages()
+                            }
+                        Toggle("Bold name", isOn: $chat.boldUsername)
+                            .onChange(of: chat.boldUsername) { _ in
+                                model.reloadChatMessages()
+                            }
+                        Toggle("Bold message", isOn: $chat.boldMessage)
+                            .onChange(of: chat.boldMessage) { _ in
+                                model.reloadChatMessages()
+                            }
+                        Toggle("Badges", isOn: $chat.badges)
+                            .onChange(of: chat.badges) { _ in
+                                model.reloadChatMessages()
+                            }
+                        Toggle("Streaming platform", isOn: $chat.platform)
+                            .onChange(of: chat.platform) { _ in
+                                model.reloadChatMessages()
+                            }
+                        Toggle("Animated emotes", isOn: $chat.animatedEmotes)
+                            .onChange(of: chat.animatedEmotes) { _ in
+                                model.reloadChatMessages()
+                            }
+                    }
+                } footer: {
+                    if model.database.showAllSettings {
+                        Text("Animated emotes are fairly CPU intensive. Disable for less power usage.")
                     }
                 }
-                Toggle(isOn: $chat.timestampColorEnabled) {
+                Section {
                     ColorPicker("Timestamp", selection: $chat.timestampColorColor, supportsOpacity: false)
                         .onChange(of: chat.timestampColorColor) { _ in
                             guard let color = chat.timestampColorColor.toRgb() else {
@@ -43,146 +75,131 @@ private struct ChatSettingsAppearanceView: View {
                             chat.timestampColor = color
                             model.reloadChatMessages()
                         }
-                }
-                .onChange(of: chat.timestampColorEnabled) { _ in
-                    model.reloadChatMessages()
-                }
-                Toggle("Bold name", isOn: $chat.boldUsername)
-                    .onChange(of: chat.boldUsername) { _ in
-                        model.reloadChatMessages()
-                    }
-                ColorPicker("Name color", selection: $chat.usernameColorColor, supportsOpacity: false)
-                    .onChange(of: chat.usernameColorColor) { _ in
-                        guard let color = chat.usernameColorColor.toRgb() else {
-                            return
-                        }
-                        chat.usernameColor = color
-                        model.reloadChatMessages()
-                    }
-                Toggle("Bold message", isOn: $chat.boldMessage)
-                    .onChange(of: chat.boldMessage) { _ in
-                        model.reloadChatMessages()
-                    }
-                ColorPicker("Message color", selection: $chat.messageColorColor, supportsOpacity: false)
-                    .onChange(of: chat.messageColorColor) { _ in
-                        guard let color = chat.messageColorColor.toRgb() else {
-                            return
-                        }
-                        chat.messageColor = color
-                        model.reloadChatMessages()
-                    }
-                Toggle("Badges", isOn: $chat.badges)
-                    .onChange(of: chat.badges) { _ in
-                        model.reloadChatMessages()
-                    }
-                Toggle("Streaming platform", isOn: $chat.platform)
-                    .onChange(of: chat.platform) { _ in
-                        model.reloadChatMessages()
-                    }
-                Toggle("Animated emotes", isOn: $chat.animatedEmotes)
-                    .onChange(of: chat.animatedEmotes) { _ in
-                        model.reloadChatMessages()
-                    }
-                Toggle(isOn: $chat.backgroundColorEnabled) {
-                    ColorPicker("Background", selection: $chat.backgroundColorColor, supportsOpacity: false)
-                        .onChange(of: chat.backgroundColorColor) { _ in
-                            guard let color = chat.backgroundColorColor.toRgb() else {
+                    ColorPicker("Name", selection: $chat.usernameColorColor, supportsOpacity: false)
+                        .onChange(of: chat.usernameColorColor) { _ in
+                            guard let color = chat.usernameColorColor.toRgb() else {
                                 return
                             }
-                            chat.backgroundColor = color
+                            chat.usernameColor = color
                             model.reloadChatMessages()
                         }
-                }
-                .onChange(of: chat.backgroundColorEnabled) { _ in
-                    model.reloadChatMessages()
-                }
-                Toggle(isOn: $chat.shadowColorEnabled) {
-                    ColorPicker("Border", selection: $chat.shadowColorColor, supportsOpacity: false)
-                        .onChange(of: chat.shadowColorColor) { _ in
-                            guard let color = chat.shadowColorColor.toRgb() else {
+                    ColorPicker("Message", selection: $chat.messageColorColor, supportsOpacity: false)
+                        .onChange(of: chat.messageColorColor) { _ in
+                            guard let color = chat.messageColorColor.toRgb() else {
                                 return
                             }
-                            chat.shadowColor = color
+                            chat.messageColor = color
                             model.reloadChatMessages()
                         }
+                    Toggle(isOn: $chat.backgroundColorEnabled) {
+                        ColorPicker("Background", selection: $chat.backgroundColorColor, supportsOpacity: false)
+                            .onChange(of: chat.backgroundColorColor) { _ in
+                                guard let color = chat.backgroundColorColor.toRgb() else {
+                                    return
+                                }
+                                chat.backgroundColor = color
+                                model.reloadChatMessages()
+                            }
+                    }
+                    .onChange(of: chat.backgroundColorEnabled) { _ in
+                        model.reloadChatMessages()
+                    }
+                    Toggle(isOn: $chat.shadowColorEnabled) {
+                        ColorPicker("Border", selection: $chat.shadowColorColor, supportsOpacity: false)
+                            .onChange(of: chat.shadowColorColor) { _ in
+                                guard let color = chat.shadowColorColor.toRgb() else {
+                                    return
+                                }
+                                chat.shadowColor = color
+                                model.reloadChatMessages()
+                            }
+                    }
+                    .onChange(of: chat.shadowColorEnabled) { _ in
+                        model.reloadChatMessages()
+                    }
+                    Toggle(isOn: Binding(get: {
+                        chat.meInUsernameColor
+                    }, set: { value in
+                        chat.meInUsernameColor = value
+                    })) {
+                        Text("Me in name color")
+                    }
+                } header: {
+                    Text("Colors")
                 }
-                .onChange(of: chat.shadowColorEnabled) { _ in
-                    model.reloadChatMessages()
-                }
-                Toggle(isOn: Binding(get: {
-                    chat.meInUsernameColor
-                }, set: { value in
-                    chat.meInUsernameColor = value
-                })) {
-                    Text("Me in name color")
-                }
-                Toggle("New messages at top", isOn: $chat.newMessagesAtTop)
-                Toggle("Mirrored", isOn: $chat.mirrored)
             }
-        } header: {
+            .navigationTitle("Appearance")
+        } label: {
             Text("Appearance")
-        } footer: {
-            if model.database.showAllSettings {
-                Text("Animated emotes are fairly CPU intensive. Disable for less power usage.")
-            }
         }
     }
 }
 
-private struct ChatSettingsGeometryView: View {
+private struct ChatSettingsLayoutView: View {
     @EnvironmentObject var model: Model
     @ObservedObject var chat: SettingsChat
 
     var body: some View {
-        Section("Geometry") {
-            HStack {
-                Text("Height")
-                Slider(
-                    value: $chat.height,
-                    in: 0.2 ... 1.0,
-                    step: 0.01,
-                    onEditingChanged: { begin in
-                        guard !begin else {
-                            return
-                        }
-                        model.reloadChatMessages()
+        NavigationLink {
+            Form {
+                Section {
+                    HStack {
+                        Text("Height")
+                        Slider(
+                            value: $chat.height,
+                            in: 0.2 ... 1.0,
+                            step: 0.01,
+                            onEditingChanged: { begin in
+                                guard !begin else {
+                                    return
+                                }
+                                model.reloadChatMessages()
+                            }
+                        )
+                        Text("\(Int(100 * chat.height))%")
+                            .frame(width: sliderValuePercentageWidth)
                     }
-                )
-                Text("\(Int(100 * chat.height))%")
-                    .frame(width: sliderValuePercentageWidth)
-            }
-            HStack {
-                Text("Width")
-                Slider(
-                    value: $chat.width,
-                    in: 0.2 ... 1.0,
-                    step: 0.01,
-                    onEditingChanged: { begin in
-                        guard !begin else {
-                            return
-                        }
-                        model.reloadChatMessages()
+                    HStack {
+                        Text("Width")
+                        Slider(
+                            value: $chat.width,
+                            in: 0.2 ... 1.0,
+                            step: 0.01,
+                            onEditingChanged: { begin in
+                                guard !begin else {
+                                    return
+                                }
+                                model.reloadChatMessages()
+                            }
+                        )
+                        Text("\(Int(100 * chat.width))%")
+                            .frame(width: sliderValuePercentageWidth)
                     }
-                )
-                Text("\(Int(100 * chat.width))%")
-                    .frame(width: sliderValuePercentageWidth)
-            }
-            HStack {
-                Text("Bottom")
-                Slider(
-                    value: $chat.bottomPoints,
-                    in: 0.0 ... 200.0,
-                    step: 5,
-                    onEditingChanged: { begin in
-                        guard !begin else {
-                            return
-                        }
-                        model.reloadChatMessages()
+                    HStack {
+                        Text("Bottom")
+                        Slider(
+                            value: $chat.bottomPoints,
+                            in: 0.0 ... 200.0,
+                            step: 5,
+                            onEditingChanged: { begin in
+                                guard !begin else {
+                                    return
+                                }
+                                model.reloadChatMessages()
+                            }
+                        )
+                        Text("\(Int(chat.bottomPoints)) pts")
+                            .frame(width: sliderValuePercentageWidth)
                     }
-                )
-                Text("\(Int(chat.bottomPoints)) pts")
-                    .frame(width: sliderValuePercentageWidth)
+                    if model.database.showAllSettings {
+                        Toggle("New messages at top", isOn: $chat.newMessagesAtTop)
+                        Toggle("Mirrored", isOn: $chat.mirrored)
+                    }
+                }
             }
+            .navigationTitle("Layout")
+        } label: {
+            Text("Layout")
         }
     }
 }
@@ -202,54 +219,50 @@ private struct ChatSettingsGeneralView: View {
     }
 
     var body: some View {
-        Section {
+        NavigationLink {
+            ChatBotSettingsView()
+        } label: {
+            Toggle(isOn: $chat.botEnabled) {
+                Text("Bot")
+            }
+        }
+        NavigationLink {
+            ChatTextToSpeechSettingsView(chat: chat)
+        } label: {
+            Toggle(isOn: Binding(get: {
+                chat.textToSpeechEnabled
+            }, set: { value in
+                chat.textToSpeechEnabled = value
+                if !value {
+                    model.chatTextToSpeech.reset(running: true)
+                }
+            })) {
+                Text("Text to speech")
+            }
+        }
+        if model.database.showAllSettings {
+            ChatFiltersSettingsView(chat: chat)
+            ChatNicknamesSettingsView(model: model, nicknames: chat.nicknames)
             NavigationLink {
-                ChatBotSettingsView()
+                TextEditView(
+                    title: String(localized: "Maximum age"),
+                    value: String(chat.maximumAge),
+                    footers: [String(localized: "Maximum message age in seconds.")]
+                ) {
+                    submitMaximumAge(value: $0)
+                }
             } label: {
-                Toggle(isOn: $chat.botEnabled) {
-                    Text("Bot")
+                Toggle(isOn: $chat.maximumAgeEnabled) {
+                    TextItemView(
+                        name: String(localized: "Maximum age"),
+                        value: String(chat.maximumAge)
+                    )
                 }
             }
-            NavigationLink {
-                ChatTextToSpeechSettingsView(chat: chat)
-            } label: {
-                Toggle(isOn: Binding(get: {
-                    chat.textToSpeechEnabled
-                }, set: { value in
-                    chat.textToSpeechEnabled = value
-                    if !value {
-                        model.chatTextToSpeech.reset(running: true)
-                    }
-                })) {
-                    Text("Text to speech")
+            Toggle("Show deleted messages", isOn: $chat.showDeletedMessages)
+                .onChange(of: chat.showDeletedMessages) { _ in
+                    model.reloadChatMessages()
                 }
-            }
-            if model.database.showAllSettings {
-                ChatFiltersSettingsView(chat: chat)
-                ChatNicknamesSettingsView(model: model, nicknames: chat.nicknames)
-                NavigationLink {
-                    TextEditView(
-                        title: String(localized: "Maximum age"),
-                        value: String(chat.maximumAge),
-                        footers: [String(localized: "Maximum message age in seconds.")]
-                    ) {
-                        submitMaximumAge(value: $0)
-                    }
-                } label: {
-                    Toggle(isOn: $chat.maximumAgeEnabled) {
-                        TextItemView(
-                            name: String(localized: "Maximum age"),
-                            value: String(chat.maximumAge)
-                        )
-                    }
-                }
-                Toggle("Show deleted messages", isOn: $chat.showDeletedMessages)
-                    .onChange(of: chat.showDeletedMessages) { _ in
-                        model.reloadChatMessages()
-                    }
-            }
-        } header: {
-            Text("General")
         }
     }
 }
@@ -278,9 +291,11 @@ struct ChatSettingsView: View {
             } header: {
                 Text("Shortcut")
             }
-            ChatSettingsAppearanceView(chat: chat)
-            ChatSettingsGeometryView(chat: chat)
-            ChatSettingsGeneralView(chat: chat)
+            Section {
+                ChatSettingsAppearanceView(chat: chat)
+                ChatSettingsLayoutView(chat: chat)
+                ChatSettingsGeneralView(chat: chat)
+            }
         }
         .navigationTitle("Chat")
     }
