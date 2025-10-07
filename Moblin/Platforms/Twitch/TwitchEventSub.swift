969ac87816c83619b806ecde8a4a5b214cf1b043
Erik Moqvist <erik.moqvist@gmail.com>
2025-10-07 19:05:13 +0200

    Do not show toast if failing to setup Twitch alerts.

--- Moblin/Platforms/Twitch/TwitchEventSub.swift
+++ Moblin/Platforms/Twitch/TwitchEventSub.swift
@@ -347,19 +347,6 @@ final class TwitchEventSub: NSObject {
         subscribeToChannelFollow()
     }
 
-    private func makeSubscribeErrorToastIfNotOk(ok: Bool, eventType: String) {
-        guard !ok else {
-            return
-        }
-        guard started else {
-            return
-        }
-        delegate
-            .twitchEventSubMakeErrorToast(
-                title: String(localized: "Failed to subscribe to Twitch \(eventType) event")
-            )
-    }
-
     private func subscribeToChannelFollow() {
         let body = createBody(
             type: subTypeChannelFollow,
@@ -367,7 +354,6 @@ final class TwitchEventSub: NSObject {
             condition: "{\"broadcaster_user_id\":\"\(userId)\",\"moderator_user_id\":\"\(userId)\"}"
         )
         twitchApi.createEventSubSubscription(body: body) { ok in
-            self.makeSubscribeErrorToastIfNotOk(ok: ok, eventType: "follow")
             guard ok else {
                 return
             }
@@ -376,31 +362,25 @@ final class TwitchEventSub: NSObject {
     }
 
     private func subscribeToChannelSubscribe() {
-        subscribeBroadcasterUserId(type: subTypeChannelSubscribe, eventType: "subscription") {
+        subscribeBroadcasterUserId(type: subTypeChannelSubscribe) {
             self.subscribeToChannelSubscriptionGift()
         }
     }
 
     private func subscribeToChannelSubscriptionGift() {
-        subscribeBroadcasterUserId(type: subTypeChannelSubscriptionGift, eventType: "subscription gift") {
+        subscribeBroadcasterUserId(type: subTypeChannelSubscriptionGift) {
             self.subscribeToChannelSubscriptionMessage()
         }
     }
 
     private func subscribeToChannelSubscriptionMessage() {
-        subscribeBroadcasterUserId(
-            type: subTypeChannelSubscriptionMessage,
-            eventType: "subscription message"
-        ) {
+        subscribeBroadcasterUserId(type: subTypeChannelSubscriptionMessage) {
             self.subscribeToChannelPointsCustomRewardRedemptionAdd()
         }
     }
 
     private func subscribeToChannelPointsCustomRewardRedemptionAdd() {
-        subscribeBroadcasterUserId(
-            type: subTypeChannelChannelPointsCustomRewardRedemptionAdd,
-            eventType: "reward redemption"
-        ) {
+        subscribeBroadcasterUserId(type: subTypeChannelChannelPointsCustomRewardRedemptionAdd) {
             self.subscribeToChannelRaid()
         }
     }
@@ -410,7 +390,6 @@ final class TwitchEventSub: NSObject {
                               version: 1,
                               condition: "{\"to_broadcaster_user_id\":\"\(userId)\"}")
         twitchApi.createEventSubSubscription(body: body) { ok in
-            self.makeSubscribeErrorToastIfNotOk(ok: ok, eventType: "raid")
             guard ok else {
                 return
             }
@@ -419,33 +398,31 @@ final class TwitchEventSub: NSObject {
     }
 
     private func subscribeToChannelCheer() {
-        subscribeBroadcasterUserId(type: subTypeChannelCheer, eventType: "cheer") {
+        subscribeBroadcasterUserId(type: subTypeChannelCheer) {
             self.subscribeToChannelHypeTrainBegin()
         }
     }
 
     private func subscribeToChannelHypeTrainBegin() {
-        subscribeBroadcasterUserId(type: subTypeChannelHypeTrainBegin, version: 2, eventType: "hype train begin") {
+        subscribeBroadcasterUserId(type: subTypeChannelHypeTrainBegin, version: 2) {
             self.subscribeToChannelHypeTrainProgress()
         }
     }
 
     private func subscribeToChannelHypeTrainProgress() {
-        subscribeBroadcasterUserId(type: subTypeChannelHypeTrainProgress, version: 2,
-                                   eventType: "hype train progress")
-        {
+        subscribeBroadcasterUserId(type: subTypeChannelHypeTrainProgress, version: 2) {
             self.subscribeToChannelHypeTrainEnd()
         }
     }
 
     private func subscribeToChannelHypeTrainEnd() {
-        subscribeBroadcasterUserId(type: subTypeChannelHypeTrainEnd, version: 2, eventType: "hype train end") {
+        subscribeBroadcasterUserId(type: subTypeChannelHypeTrainEnd, version: 2) {
             self.subscribeTochannelAdBreakBegin()
         }
     }
 
     private func subscribeTochannelAdBreakBegin() {
-        subscribeBroadcasterUserId(type: subTypeChannelAdBreakBegin, eventType: "ad break begin") {
+        subscribeBroadcasterUserId(type: subTypeChannelAdBreakBegin) {
             self.connected = true
         }
     }
@@ -453,12 +430,10 @@ final class TwitchEventSub: NSObject {
     private func subscribeBroadcasterUserId(
         type: String,
         version: Int = 1,
-        eventType: String,
         onSuccess: @escaping () -> Void
     ) {
         let body = createBroadcasterUserIdBody(type: type, version: version)
         twitchApi.createEventSubSubscription(body: body) { ok in
-            self.makeSubscribeErrorToastIfNotOk(ok: ok, eventType: eventType)
             guard ok else {
                 return
             }
