2a5bb53a288046259c716ca40d9689dec8137da3
Erik Moqvist <erik.moqvist@gmail.com>
2025-10-09 11:56:39 +0200

    Refactoring.

---
+++ Moblin/View/Settings/Scenes/Widgets/Widget/Effects/RemoveBackgroundView.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+struct RemoveBackgroundView: View {
+    @EnvironmentObject var model: Model
+    let widgetId: UUID
+    let effectIndex: Int?
+    @ObservedObject var removeBackground: SettingsVideoEffectRemoveBackground
+
+    private func updateWidget() {
+        guard let effectIndex, let effect = model.getEffectWithPossibleEffects(id: widgetId) else {
+            return
+        }
+        guard effectIndex < effect.effects.count else {
+            return
+        }
+        guard let effect = effect.effects[effectIndex] as? RemoveBackgroundEffect else {
+            return
+        }
+        effect.setColorRange(from: removeBackground.from, to: removeBackground.to)
+    }
+
+    var body: some View {
+        Section {
+            ColorPicker("From", selection: $removeBackground.fromColor, supportsOpacity: false)
+                .onChange(of: removeBackground.fromColor) { _ in
+                    guard let color = removeBackground.fromColor.toRgb() else {
+                        return
+                    }
+                    removeBackground.from = color
+                    updateWidget()
+                }
+            ColorPicker("To", selection: $removeBackground.toColor, supportsOpacity: false)
+                .onChange(of: removeBackground.toColor) { _ in
+                    guard let color = removeBackground.toColor.toRgb() else {
+                        return
+                    }
+                    removeBackground.to = color
+                    updateWidget()
+                }
+        } header: {
+            Text("Color range")
+        }
+    }
+}
---
+++ Moblin/View/Settings/Scenes/Widgets/Widget/Effects/WidgetEffectsView.swift
@@ -0,0 +1,90 @@
+import SwiftUI
+
+private struct EffectView: View {
+    @EnvironmentObject var model: Model
+    let widgetId: UUID
+    let effectIndex: Int?
+    @ObservedObject var effect: SettingsVideoEffect
+
+    var body: some View {
+        NavigationLink {
+            Form {
+                Section {
+                    Picker("Type", selection: $effect.type) {
+                        ForEach(SettingsVideoEffectType.allCases, id: \.self) {
+                            Text($0.toString())
+                                .tag($0)
+                        }
+                    }
+                    .onChange(of: effect.type) { _ in
+                        model.resetSelectedScene(changeScene: false)
+                    }
+                }
+                switch effect.type {
+                case .removeBackground:
+                    RemoveBackgroundView(
+                        widgetId: widgetId,
+                        effectIndex: effectIndex,
+                        removeBackground: effect.removeBackground
+                    )
+                case .shape:
+                    ShapeView(
+                        widgetId: widgetId,
+                        effectIndex: effectIndex,
+                        shape: effect.shape
+                    )
+                case .dewarp360:
+                    Dewarp360View(
+                        widgetId: widgetId,
+                        effectIndex: effectIndex,
+                        dewarp360: effect.dewarp360
+                    )
+                default:
+                    EmptyView()
+                }
+            }
+            .navigationTitle(effect.type.toString())
+        } label: {
+            HStack {
+                DraggableItemPrefixView()
+                Toggle(effect.type.toString(), isOn: $effect.enabled)
+                    .onChange(of: effect.enabled) { _ in
+                        model.resetSelectedScene(changeScene: false)
+                    }
+            }
+        }
+    }
+}
+
+struct WidgetEffectsView: View {
+    @EnvironmentObject var model: Model
+    @ObservedObject var widget: SettingsWidget
+
+    var body: some View {
+        Section {
+            ForEach(widget.effects) { effect in
+                EffectView(
+                    widgetId: widget.id,
+                    effectIndex: widget.effects.filter { $0.enabled }.firstIndex(where: { $0 === effect }),
+                    effect: effect
+                )
+            }
+            .onMove { froms, to in
+                widget.effects.move(fromOffsets: froms, toOffset: to)
+                model.resetSelectedScene(changeScene: false)
+            }
+            .onDelete { offsets in
+                widget.effects.remove(atOffsets: offsets)
+                model.resetSelectedScene(changeScene: false)
+            }
+            AddButtonView {
+                widget.effects.append(SettingsVideoEffect())
+                model.resetSelectedScene(changeScene: false)
+            }
+        } header: {
+            Text("Effects")
+        } footer: {
+            SwipeLeftToDeleteHelpView(kind: String(localized: "an effect"))
+        }
+    }
+}
