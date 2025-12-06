9304c94a98eb30a9661b5f34d5f2ced56cca864f
Erik Moqvist <erik.moqvist@gmail.com>
2025-12-06 16:53:59 +0100

    Try to browser widget vidoes show on screen using a canvas.

--- Moblin/VideoEffects/Browser/BrowserEffect.swift
+++ Moblin/VideoEffects/Browser/BrowserEffect.swift
@@ -25,24 +25,102 @@ private func createStyleSheetSource(styleSheet: String) -> String? {
     """
 }
 
-private func ensureVideoPlaysInlineScript() -> String {
+private func videoScript() -> String {
     return """
-    function ensureVideoPlaysInline() {
-      document.querySelectorAll('video').forEach(video => {
-        video.setAttribute('playsinline', "");
+    class MoblinVideoOnCanvasDrawer {
+      constructor(video) {
+        this.video = video;
+        this.canvas = document.createElement("canvas");
+        this.canvasContext = this.canvas.getContext("2d");
+        this.video.requestVideoFrameCallback(this.handleVideoFrame);
+        document.body.appendChild(this.canvas);
+        this.timer = setInterval(() => {
+          this.handleTimer();
+        }, 100);
+      }
+
+      tearDown = () => {
+        document.body.removeChild(this.canvas);
+        this.video = null;
+        this.canvas = null;
+        this.canvasContext = null;
+        clearInterval(this.timer);
+        this.timer = null
+      };
+
+      handleTimer = () => {
+        if (this.video?.paused) {
+          this.canvas.width = 0;
+          this.canvas.height = 0;
+        }
+      };
+
+      positionCanvas = () => {
+        const rect = this.video.getBoundingClientRect();
+        this.canvas.width = this.video.videoWidth;
+        this.canvas.height = this.video.videoHeight;
+        this.canvas.style.position = "absolute";
+        this.canvas.style.left = rect.left + window.scrollX + "px";
+        this.canvas.style.top = rect.top + window.scrollY + "px";
+        this.canvas.style.width = rect.width + "px";
+        this.canvas.style.height = rect.height + "px";
+        this.canvas.style.zIndex = -9999;
+      };
+
+      handleVideoFrame = (now, metadata) => {
+        if (this.canvasContext === null) {
+          return;
+        }
+        this.positionCanvas();
+        this.canvasContext.drawImage(
+          this.video,
+          0,
+          0,
+          this.canvas.width,
+          this.canvas.height
+        );
+        this.video.requestVideoFrameCallback(this.handleVideoFrame);
+      };
+    }
+
+    function moblinUpdateVideosPlaysInline() {
+      document.querySelectorAll("video").forEach((video) => {
+        video.setAttribute("playsinline", "");
       });
     }
 
-    const moblinObserver = new MutationObserver((mutationList, observer) => {
-      for (const mutation of mutationList) {
-        console.log("xxx", mutation);
+    let moblinVideoOnCanvasDrawers = new Map();
+
+    function moblinUpdateVideoOnCanvasDrawers() {
+      const videos = [...document.querySelectorAll("video")];
+      videos.forEach((video) => {
+        if (moblinVideoOnCanvasDrawers.get(video) === undefined) {
+          moblinVideoOnCanvasDrawers.set(
+            video,
+            new MoblinVideoOnCanvasDrawer(video)
+          );
+        }
+      });
+      for (const video of moblinVideoOnCanvasDrawers.keys()) {
+        if (!videos.includes(video)) {
+          moblinVideoOnCanvasDrawers.get(video).tearDown();
+          moblinVideoOnCanvasDrawers.delete(video);
+        }
       }
-      ensureVideoPlaysInline();
+    }
+
+    function moblinUpdateVideosConfigured() {
+      moblinUpdateVideosPlaysInline();
+      moblinUpdateVideoOnCanvasDrawers();
+    }
+
+    const moblinObserver = new MutationObserver((mutationList, observer) => {
+      moblinUpdateVideosConfigured();
     });
     moblinObserver.observe(document, { childList: true, subtree: true });
 
     document.addEventListener("DOMContentLoaded", (event) => {
-      ensureVideoPlaysInline();
+      moblinUpdateVideosConfigured();
     });
     """
 }
@@ -111,7 +189,7 @@ final class BrowserEffect: VideoEffect {
         if let source = createStyleSheetSource(styleSheet: styleSheet) {
             addScript(configuration, source, .atDocumentEnd)
         }
-        addScript(configuration, ensureVideoPlaysInlineScript(), .atDocumentStart)
+        addScript(configuration, videoScript(), .atDocumentStart)
         server = BrowserEffectServer()
         if moblinAccess {
             server.addScript(configuration: configuration)
