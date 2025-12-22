ca2bbb41d4500b8cf4ad26768e71d3fd87645919
Erik Moqvist <erik.moqvist@gmail.com>
2025-12-22 08:41:21 +0100

    Blur kernel experiment. Not very successful.

---
+++ Moblin/VideoEffects/Blur/BlurFilter.swift
@@ -0,0 +1,15 @@
+import CoreImage
+
+class BlurFilter: CIFilter {
+    var inputImage: CIImage?
+    var radius: Float = 8
+
+    override var outputImage: CIImage? {
+        guard let inputImage else {
+            return nil
+        }
+        return try? BlurKernel.apply(withExtent: inputImage.extent,
+                                                inputs: [inputImage],
+                                                arguments: ["radius": radius])
+    }
+}
---
+++ Moblin/VideoEffects/Blur/BlurKernel.swift
@@ -0,0 +1,23 @@
+import CoreImage
+import Metal
+import MetalPerformanceShaders
+
+final class BlurKernel: CIImageProcessorKernel {
+    override class func process(with inputs: [CIImageProcessorInput]?,
+                                arguments: [String : Any]?,
+                                output: CIImageProcessorOutput) throws
+    {
+        guard let commandBuffer = output.metalCommandBuffer,
+              let input = inputs?.first,
+              let sourceTexture = input.metalTexture,
+              let destinationTexture = output.metalTexture,
+              let radius = arguments?["radius"] as? Float else {
+            return
+        }
+        let sigma = radius
+        let blur = MPSImageGaussianBlur(device: commandBuffer.device, sigma: sigma)
+        blur.encode(commandBuffer: commandBuffer,
+                    sourceTexture: sourceTexture,
+                    destinationTexture: destinationTexture)
+    }
+}
