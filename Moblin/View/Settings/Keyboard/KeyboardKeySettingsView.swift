f0639bab91b2007381c356f7d9b86424e2dccb2f
Erik Moqvist <erik.moqvist@gmail.com>
2025-12-29 20:15:27 +0100

    Keyboard keys must be one letter.

--- Moblin/View/Settings/Keyboard/KeyboardKeySettingsView.swift
+++ Moblin/View/Settings/Keyboard/KeyboardKeySettingsView.swift
@@ -1,5 +1,46 @@
 import SwiftUI
 
+private struct SelectedKeyView: View {
+    @ObservedObject var key: SettingsKeyboardKey
+
+    var body: some View {
+        if key.key.isEmpty {
+            Text("No key set")
+                .foregroundStyle(.gray)
+        } else {
+            Text(key.key)
+        }
+    }
+}
+
+private struct KeyPickerView: View {
+    @ObservedObject var key: SettingsKeyboardKey
+    @FocusState var editingText: Bool
+    @Environment(\.dismiss) var dismiss
+
+    var body: some View {
+        TextField("No key set", text: $key.key)
+            .textInputAutocapitalization(.never)
+            .autocorrectionDisabled()
+            .focused($editingText)
+            .onChange(of: key.key) { _ in
+                guard !key.key.isEmpty else {
+                    return
+                }
+                dismiss()
+            }
+            .onSubmit {
+                dismiss()
+            }
+            .onChange(of: editingText) { _ in
+                guard editingText else {
+                    return
+                }
+                key.key = ""
+            }
+    }
+}
+
 struct KeyboardKeySettingsView: View {
     @EnvironmentObject var model: Model
     @ObservedObject var key: SettingsKeyboardKey
@@ -32,11 +73,19 @@ struct KeyboardKeySettingsView: View {
         NavigationLink {
             Form {
                 Section {
-                    TextEditNavigationView(title: String(localized: "Key"),
-                                           value: key.key,
-                                           onSubmit: {
-                                               key.key = $0
-                                           })
+                    NavigationLink {
+                        Form {
+                            KeyPickerView(key: key)
+                        }
+                        .navigationTitle("Key")
+                    } label: {
+                        HStack {
+                            Text("Key")
+                            Spacer()
+                            SelectedKeyView(key: key)
+                                .foregroundStyle(.gray)
+                        }
+                    }
                 }
                 Section {
                     NavigationLink {
@@ -86,12 +135,7 @@ struct KeyboardKeySettingsView: View {
             .navigationTitle("Keyboard key")
         } label: {
             HStack {
-                if key.key.isEmpty {
-                    Text("No key set")
-                        .foregroundStyle(.gray)
-                } else {
-                    Text(key.key)
-                }
+                SelectedKeyView(key: key)
                 Spacer()
                 Text(keyText())
                     .foregroundStyle(keyColor())
