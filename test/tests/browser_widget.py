b1955fbba9d034148d3f0761be5c05523cfec58f
Erik Moqvist <erik.moqvist@gmail.com>
2026-07-04 20:17:11 +0200

    Refactoring.

--- test/tests/browser_widget.py
+++ test/tests/browser_widget.py
@@ -1,7 +1,10 @@
 import logging
+from pathlib import Path
 import time
+from typing import List
 
 from utils.ffmpeg import create_qr_codes_video
+from utils.ffmpeg import QrCode
 from utils.ffmpeg import read_qr_codes
 from utils.utils import WEBSITES_ROOT
 from utils.utils import Crop
@@ -34,30 +37,36 @@ class BrowserWidgetHighFpsVideo(TestCase):
             recording_file = self.moblin.download_and_delete_latest_recording(
                 "BrowserWidgetHighFpsVideo.mp4"
             )
-            image_crop = Crop(x=0, y=0, width=400, height=400)
-            image_qr_codes = read_qr_codes(recording_file, image_crop)
-            for qr_code in image_qr_codes[:100]:
-                self.assert_equal(qr_code.number, -1)
-            for qr_code in image_qr_codes[150:380]:
-                self.assert_not_equal(qr_code.number, -1)
-            for qr_code in image_qr_codes[450:]:
-                self.assert_equal(qr_code.number, -1)
-            video_crop = Crop(x=400, y=0, width=400, height=400)
-            video_qr_codes = read_qr_codes(recording_file, video_crop)
-            for qr_code in video_qr_codes[:100]:
-                self.assert_equal(qr_code.number, -1)
-            previous_frame_number = video_qr_codes[149].number
-            seen_frame_number_count = 1
-            for qr_code in video_qr_codes[150:380]:
-                if qr_code.number == previous_frame_number:
-                    seen_frame_number_count += 1
-                else:
-                    seen_frame_number_count = 1
-                self.assert_greater_equal(qr_code.number, previous_frame_number)
-                self.assert_less(seen_frame_number_count, 4)
-                previous_frame_number = qr_code.number
-            for qr_code in video_qr_codes[450:]:
-                self.assert_equal(qr_code.number, -1)
+            self.assert_image_qr_codes(recording_file)
+            self.assert_video_qr_codes(recording_file)
+
+    def assert_image_qr_codes(self, recording_file: Path):
+        crop = Crop(x=0, y=0, width=400, height=400)
+        qr_codes = read_qr_codes(recording_file, crop)
+        self.assert_no_qr_codes_found(qr_codes[:100])
+        for qr_code in qr_codes[150:380]:
+            self.assert_not_equal(qr_code.number, -1)
+        self.assert_no_qr_codes_found(qr_codes[450:])
+
+    def assert_video_qr_codes(self, recording_file: Path):
+        crop = Crop(x=400, y=0, width=400, height=400)
+        qr_codes = read_qr_codes(recording_file, crop)
+        self.assert_no_qr_codes_found(qr_codes[:100])
+        previous_frame_number = qr_codes[149].number
+        seen_frame_number_count = 1
+        for qr_code in qr_codes[150:380]:
+            if qr_code.number == previous_frame_number:
+                seen_frame_number_count += 1
+            else:
+                seen_frame_number_count = 1
+            self.assert_greater_equal(qr_code.number, previous_frame_number)
+            self.assert_less(seen_frame_number_count, 4)
+            previous_frame_number = qr_code.number
+        self.assert_no_qr_codes_found(qr_codes[450:])
+
+    def assert_no_qr_codes_found(self, qr_codes: List[QrCode]):
+        for qr_code in qr_codes:
+            self.assert_equal(qr_code.number, -1)
 
 
 class BrowserWidgetScriptDefer(TestCase):
