60e0d92f3691ae39ddae131e67b8f0f77601975d
Erik Moqvist <erik.moqvist@gmail.com>
2026-08-07 19:07:26 +0200

    libdatachannel logging function.

--- Moblin/Media/Webrtc/WebrtcCommon.swift
+++ Moblin/Media/Webrtc/WebrtcCommon.swift
@@ -1,1 +1,12 @@
+import libdatachannel
+
 let defaultStunServer = "stun:stun.l.google.com:19302"
+
+func setupWebrtcDebugLogging() {
+    rtcInitLogger(RTC_LOG_DEBUG) { _, message in
+        guard let message else {
+            return
+        }
+        logger.info("webrtc: \(String(cString: message))")
+    }
+}
