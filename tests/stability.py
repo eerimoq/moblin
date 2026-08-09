fcb1da5e199e6701aa1ef5db7a5c91c3cff8a69b
Erik Moqvist <erik.moqvist@gmail.com>
2026-08-09 11:23:03 +0200

    Refactoring.

--- tests/stability.py
+++ tests/stability.py
@@ -4,6 +4,10 @@ from utils.runner import create_parser
 from utils.runner import run
 
 
+def create_suites(moblin, args):
+    return [stability.tests(moblin, 3600 * args.duration)]
+
+
 def main():
     parser = create_parser("Run the app for a long time and monitor it.")
     parser.add_argument(
@@ -12,11 +16,7 @@ def main():
         default=stability.DEFAULT_DURATION / 3600,
         help="Duration in hours (default: %(default)s).",
     )
-    run(
-        "stability",
-        parser,
-        lambda moblin, args: [stability.tests(moblin, 3600 * args.duration)],
-    )
+    run("stability", parser, create_suites)
 
 
 main()
--- tests/test.py
+++ tests/test.py
@@ -10,20 +10,20 @@ from utils.runner import create_parser
 from utils.runner import run
 
 
+def create_suites(moblin, _):
+    return [
+        talkback.tests(moblin),
+        ingests.tests(moblin),
+        record.tests(moblin),
+        scenes.tests(moblin),
+        stream.tests(moblin),
+        browser_widget.tests(moblin),
+        web_remote_control.tests(moblin),
+    ]
+
+
 def main():
-    run(
-        "test",
-        create_parser(),
-        lambda moblin, _: [
-            talkback.tests(moblin),
-            ingests.tests(moblin),
-            record.tests(moblin),
-            scenes.tests(moblin),
-            stream.tests(moblin),
-            browser_widget.tests(moblin),
-            web_remote_control.tests(moblin),
-        ],
-    )
+    run("test", create_parser(), create_suites)
 
 
 main()
