0d6ebd82a7011ffec85d234c4b8492198b96116c
Erik Moqvist <erik.moqvist@gmail.com>
2026-09-05 11:23:44 +0200

    Smaller big GIFs in chat widget.

--- Moblin/VideoEffects/ChatEffect.swift
+++ Moblin/VideoEffects/ChatEffect.swift
@@ -176,7 +176,7 @@ private struct LineView: View {
                     ChatGifView(
                         url: url,
                         animated: false,
-                        height: CGFloat(settings.fontSize * chatEmoteScale * 5)
+                        height: CGFloat(settings.fontSize * chatEmoteScale * 3)
                     )
                     .padding(.vertical, settings.shadowColorEnabled ? 1.5 : 0)
                     Text(" ")
