f186535fa037e51371fb666b2fa80bbc2e8f5c9b
Erik Moqvist <erik.moqvist@gmail.com>
2026-09-11 14:02:54 +0200

    Refactoring.

--- Moblin/View/ControlBar/RemoteControlAssistant/ControlBarRemoteControlAssistantView.swift
+++ Moblin/View/ControlBar/RemoteControlAssistant/ControlBarRemoteControlAssistantView.swift
@@ -100,10 +100,6 @@ private struct RemoteControlAudioLevelView: View {
     var level: Float
     var channels: Int?
     private let barsPerDb: Float = 0.3
-    private let clippingThresholdDb: Float = -0.2
-    private let redThresholdDb: Float = -8.5
-    private let yellowThresholdDb: Float = -20
-    private let zeroThresholdDb: Float = -60
 
     // Approx 60 * 0.3 = 20
     private let maxBars = "||||||||||||||||||||"
