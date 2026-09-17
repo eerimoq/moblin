4b0217b062db0608f52e61fbe49e9ee84c706d7a
gabbelitoV2 <128819414+gabbelitoV2@users.noreply.github.com>
2026-09-17 23:05:44 +0200

    Lowercase Twitch channel name when joining chat. (#449)

--- Moblin/StreamingPlatforms/Twitch/TwitchChat.swift
+++ Moblin/StreamingPlatforms/Twitch/TwitchChat.swift
@@ -746,7 +746,7 @@ extension TwitchChat: WebSocketClientDelegate {
         webSocket.send(string: "CAP REQ :twitch.tv/commands")
         webSocket.send(string: "PASS oauth:SCHMOOPIIE")
         webSocket.send(string: "NICK justinfan67420")
-        webSocket.send(string: "JOIN #\(channelName)")
+        webSocket.send(string: "JOIN #\(channelName.lowercased())")
     }
 
     func webSocketClientDisconnected(_: WebSocketClient) {
